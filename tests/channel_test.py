f3e672ff49f72837a383c121467411024be64328
Joshua Cruzado <z5367218@ad.unsw.edu.au>
2021-09-26 21:41:44 +1000

    Set up structure for the tests

---
+++ tests/channel_test.py
@@ -0,0 +1,16 @@
+import pytest
+from src.auth import auth_register_v1
+from src.channel import channel
+from src.other import other
+
+# channel_messages_v1 tests
+# test - errors
+# channel_valid, start_valid, member_valid
+def test_ch_mess_error_channel-invalid():
+    pass
+def test_ch_mess_error_start-invalid():
+    pass
+def test_ch_mess_error_member-invalid():
+    pass
+# test - valid scenarios
+def test_ch_mess():
