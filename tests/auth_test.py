94c74cc873a0d18dc8028eba24e006d1d766bc4f
Julian Winzer <z5371533@ad.unsw.edu.au>
2021-09-26 20:32:27 +1000

    Added comments

--- tests/auth_test.py
+++ tests/auth_test.py
@@ -3,13 +3,16 @@ import pytest
 from src.auth import auth_register_v1
 from src.error import InputError
 
+#Invalid input tests:
+
+#Test for InputError when email, password, first name or last name are invalid
 def test_register_invalid_email():
     with pytest.raises(InputError):
         auth_register_v1("email", "password", "John", "Smith")
 
 def test_register_invalid_password():
     with pytest.raises(InputError):
-        auth_register_v1("email@email.com", "pa", "John", "Smith")
+        auth_register_v1("email@email.com", "pass", "John", "Smith")
 
 
 def test_register_invalid_fName():
@@ -20,5 +23,10 @@ def test_register_invalid_lName():
     with pytest.raises(InputError):
         auth_register_v1("email@email.com", "pass", "John", "")
 
+def test_register_invalid_multiple():
+    with pytest.raises(InputError):
+        auth_register_v1("email", "pass", "John", "")
+
+#Valid input test
 def test_valid_input():
     assert auth_register_v1("email@email.com", "pass", "John", "Smith") == 1
