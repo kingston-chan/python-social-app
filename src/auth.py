1f1733cc40d1d3eaf6bfd134ac31cc0536c690ea
Julian Winzer <z5371533@ad.unsw.edu.au>
2021-09-26 17:41:28 +1000

    Branch test

--- src/auth.py
+++ src/auth.py
@@ -1,5 +1,6 @@
 from src.data_store import data_store
 from src.error import InputError
+import re
 
 def auth_login_v1(email, password):
     return {
@@ -7,6 +8,26 @@ def auth_login_v1(email, password):
     }
 
 def auth_register_v1(email, password, name_first, name_last):
+    
+    if re.match(r"'^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}$'", email) and len(password) >= 6 and 1 <= len(name_first) <= 50 and 1 <= len(name_last) <= 50:
+        handle = name_first.lower() + name_last.lower()
+        ''.join([x for x in handle if x.isalnum()])
+        
+        if len(handle) > 20:
+            handle = handle[:20]
+
+        store = data_store.get()
+
+        if handle in store.items():
+            pass
+
+
+    else:
+        raise InputError('Invalid registration')
+
+
+
+
     return {
         'auth_user_id': 1,
     }
