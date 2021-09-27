70582da2e4b42cebe3a259fdc0af1b7a26617d73
z5360545 <z5360545@cse.unsw.edu.au>
2021-09-27 15:11:41 +1000

    Added functionality for channels_listall

--- src/channels.py
+++ src/channels.py
@@ -1,22 +1,38 @@
+from src.data_store import data_store
+from src.error import AccessError
+
+store = data_store.get()
+channels = store['channels']
+users = store['users']
+
 def channels_list_v1(auth_user_id):
     return {
         'channels': [
-        	{
-        		'channel_id': 1,
-        		'name': 'My Channel',
-        	}
+            {
+                'channel_id': 1,
+                'name': 'My Channel',
+            }
         ],
     }
 
 def channels_listall_v1(auth_user_id):
-    return {
-        'channels': [
-        	{
-        		'channel_id': 1,
-        		'name': 'My Channel',
-        	}
-        ],
-    }
+    user_exists = 0
+    for user in users:
+        if user['id'] == auth_user_id:
+            user_exists = 1
+    
+    if not user_exists:
+        raise AccessError("User does not exist")
+    
+    channels_dict = {}
+    channels_dict['channel_id'] = []
+    channels_dict['name'] = []
+
+    for channel in channels:
+        channels_dict['channel_id'].append(channel['id'])
+        channels_dict['name'].append(channel['name'])
+
+    return channels_dict
 
 def channels_create_v1(auth_user_id, name, is_public):
     return {
