49727f53c3645e5ab8d0006a630dec8628471b7e
Julian Winzer <z5371533@ad.unsw.edu.au>
2021-09-27 14:05:59 +1000

    Added several more tests for valid inputs

--- tests/auth_test.py
+++ tests/auth_test.py
@@ -2,6 +2,7 @@ import pytest
 
 from src.auth import auth_register_v1
 from src.error import InputError
+from src.other import clear_v1
 
 #Invalid input tests:
 
@@ -27,6 +28,27 @@ def test_register_invalid_multiple():
     with pytest.raises(InputError):
         auth_register_v1("email", "pass", "John", "")
 
-#Valid input test
+#Valid input tests:
+
 def test_valid_input():
-    assert auth_register_v1("email@email.com", "pass", "John", "Smith") == 1
+    clear_v1()
+    assert auth_register_v1("email@email.com", "password", "John", "Smith") == {'auth_user_id': 1}
+
+def test_valid_numbers():
+    clear_v1()
+    assert auth_register_v1("161998@439876.com", "2354335425", "24352345", "34553") == {'auth_user_id': 1}
+
+def test_symbols_in_name():
+    clear_v1()
+    assert auth_register_v1("email@email.com", "password", "Jo!@#$%^&*hn", "Smith") == {'auth_user_id': 1}
+
+def test_spaces():
+    clear_v1()
+    assert auth_register_v1("email@email.com", "pass  word", "J o h n ", " S m i t h") == {'auth_user_id': 1}
+
+
+def test_valid_multiple_identical():
+    clear_v1()
+    assert auth_register_v1("email@email.com", "password", "John", "Smith") == {'auth_user_id': 1}
+    assert auth_register_v1("email@email.com", "password", "John", "Smith") == {'auth_user_id': 2}
+    assert auth_register_v1("email@email.com", "password", "John", "Smith") == {'auth_user_id': 3}
