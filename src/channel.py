1ad2cdea2810bacf6c3d1d6b3deadbf06cfa0d1f
z5360545 <z5360545@cse.unsw.edu.au>
2021-09-27 15:14:43 +1000

    Added functionality for channel_details

--- src/channel.py
+++ src/channel.py
@@ -1,29 +1,49 @@
+from src.data_store import data_store
+from src.error import AccessError
+from src.error import InputError
+
+
 def channel_invite_v1(auth_user_id, channel_id, u_id):
     return {
     }
 
 def channel_details_v1(auth_user_id, channel_id):
-    return {
-        'name': 'Hayden',
-        'owner_members': [
-            {
-                'u_id': 1,
-                'email': 'example@gmail.com',
-                'name_first': 'Hayden',
-                'name_last': 'Jacobs',
-                'handle_str': 'haydenjacobs',
-            }
-        ],
-        'all_members': [
-            {
-                'u_id': 1,
-                'email': 'example@gmail.com',
-                'name_first': 'Hayden',
-                'name_last': 'Jacobs',
-                'handle_str': 'haydenjacobs',
-            }
-        ],
-    }
+    store = data_store.get()
+    channels = store['channels']
+    users = store['users']
+    channel_details = {}
+
+    user_exists = 0
+    for user in users:
+        if user['id'] == auth_user_id:
+            user_exists = 1
+    
+    if not user_exists:
+        raise AccessError("User does not exist")
+    
+    channel_exists = 0
+    for channel in channels:
+        if channel["id"] == channel_id:
+            channel_exists = 1
+            channel_details["name"] = channel["name"]
+            channel_details["is_public"] = channel["is_public"]
+            channel_details["owner_members"] = channel["owner_members"]
+            channel_details["all_members"] = channel["all_members"]
+
+    if not channel_exists:
+        raise InputError("Channel does not exist")
+    
+    an_invited_member = 0
+
+    members = channel_details["all_members"]
+    for member in members["id"]:
+        if member == auth_user_id:
+            an_invited_member = 1
+
+    if not an_invited_member:
+        raise AccessError("User is not a member of the channel")
+    
+    return channel_details
 
 def channel_messages_v1(auth_user_id, channel_id, start):
     return {
