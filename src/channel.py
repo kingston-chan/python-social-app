90bfc7b175051c2781202b55c966c420c7a29bc7
Joshua Cruzado <z5367218@ad.unsw.edu.au>
2021-09-27 15:48:37 +1000

    Improved imports, changed variables, improved tests

--- src/channel.py
+++ src/channel.py
@@ -1,3 +1,6 @@
+from src.data_store import data_store
+from src.error import InputError, AccessError
+
 def channel_invite_v1(auth_user_id, channel_id, u_id):
     return {
     }
@@ -26,19 +29,53 @@ def channel_details_v1(auth_user_id, channel_id):
     }
 
 def channel_messages_v1(auth_user_id, channel_id, start):
-    return {
-        'messages': [
-            {
-                'message_id': 1,
-                'u_id': 1,
-                'message': 'Hello world',
-                'time_created': 1582426789,
-            }
-        ],
-        'start': 0,
-        'end': 50,
-    }
+    # checks if channel and start are valid inputs
+    channel_valid = False
+    start_valid = False
+    selected_channel = {}
+    store = data_store.get()
+    channels = store['channels']
+    for channel in channels:
+        if channel["channel_id"] == channel_id:
+            channel_valid = True
+            if start <= len(channel["messages"]):
+                start_valid = True
+                selected_channel = channel
 
+    if channel_valid is False:
+        raise InputError("This channel is not valid.")
+    if start_valid is False:
+        raise InputError("This start is not valid.")
+    
+    member_valid = False
+    for member in selected_channel[all_members]:
+        if member["u_id"] == auth_user_id:
+            member_valid = True
+    
+    if member_valid is False:
+        raise AccessError("User is not authorised.")
+
+    index = start
+    counter = 0
+    messages = selected_channel[messages]
+    selected_messages = []
+    while index <= len(messages) and counter < 50:
+        for message in messages:
+            if message["message_id"] == index:
+                selected_messages.append(message)
+        index += 1
+        counter += 1
+
+    if counter != 50:
+        end = -1
+    else:
+        end = index
+
+    return {
+        'messages': selected_messages,
+        'start': start,
+        'end': end,
+    }
 def channel_join_v1(auth_user_id, channel_id):
     return {
     }
--- tests/channel_test.py
+++ tests/channel_test.py
@@ -1,100 +1,85 @@
 import pytest
 from src.auth import auth_register_v1
-from src.channel import channel
-from src.other import other
+from src.channels import channels_create_v1
+from src.channel import channel_messages_v1
+from src.other import clear_v1
+from src.error import InputError, AccessError
 
 # channel_messages_v1 tests
+
 # test - errors
 # channel_valid, start_valid, member_valid
-def test_ch_mess_error_channel-invalid():
+def test_ch_mess_error_channel_invalid():
     clear_v1()
     # make user
-    auth_register_v1(
+    user = auth_register_v1(
         "fakeguy@fakeemail.com",
-        "fakepassword"
-        "fakefirstname"
-        "fakelastname"
-    )
-    id = auth_login_v1(
-        "fakeguy@fakeemail.com"
-        "fakepassword"
+        "fakepassword",
+        "fakefirstname",
+        "fakelastname",
     )
     # no channel created
     # function
     with pytest.raises(InputError):
-        channel_messages_v1(id, 0, 0)
+        channel_messages_v1(user["auth_user_id"], 0, 0)
 
-def test_ch_mess_error_start-invalid():
+def test_ch_mess_error_start_invalid():
     clear_v1()
     # make user
-    auth_register_v1(
+    user = auth_register_v1(
         "fakeguy@fakeemail.com",
-        "fakepassword"
-        "fakefirstname"
-        "fakelastname"
-    )
-    id = auth_login_v1(
-        "fakeguy@fakeemail.com"
-        "fakepassword"
+        "fakepassword",
+        "fakefirstname",
+        "fakelastname",
     )
     # make channel
-    ch_id = channels_create(
-        id,
+    ch_id = channels_create_v1(
+        user["auth_user_id"],
         "random_channel_name",
-        True
+        True,
     )
     # function
     with pytest.raises(InputError):
-        channel_messages_v1(id, ch_id, 100)
+        channel_messages_v1(user["auth_user_id"], ch_id, 100)
 
-def test_ch_mess_error_member-invalid():
+def test_ch_mess_error_member_invalid():
     clear_v1()
     # make user
-    auth_register_v1(
+    user = auth_register_v1(
         "fakeguy@fakeemail.com",
-        "fakepassword"
-        "fakefirstname"
-        "fakelastname"
-    )
-    id = auth_login_v1(
-        "fakeguy@fakeemail.com"
-        "fakepassword"
+        "fakepassword",
+        "fakefirstname",
+        "fakelastname",
     )
     # make channel
-    ch_id = channels_create(
-        id,
+    ch_id = channels_create_v1(
+        user["auth_user_id"],
         "random_channel_name",
-        True
+        True,
     )
     # function
-    with pytest.raises(InputError):
-        channel_messages_v1(id + 1, ch_id, 100)
+    with pytest.raises(AccessError):
+        channel_messages_v1((user["auth_user_id"] + 1), ch_id, 0)
 
 # test - valid scenarios
 def test_ch_mess():
     clear_v1()
-        clear_v1()
     # make user
-    auth_register_v1(
+    user = auth_register_v1(
         "fakeguy@fakeemail.com",
-        "fakepassword"
-        "fakefirstname"
-        "fakelastname"
-    )
-    id = auth_login_v1(
-        "fakeguy@fakeemail.com"
-        "fakepassword"
+        "fakepassword",
+        "fakefirstname",
+        "fakelastname",
     )
     # make channel
-    ch_id = channels_create(
-        id,
+    ch_id = channels_create_v1(
+        user["auth_user_id"],
         "random_channel_name",
-        True
+        True,
     )
     # function
-    assert channel_messages_v1(id, ch_id, 0) == {
-        'messages':
-        'start': 0
-        'end': -1
+    assert channel_messages_v1(user["auth_user_id"], ch_id, 0) == {
+        'messages': [],
+        'start': 0,
+        'end': -1,
     }
-    pass
