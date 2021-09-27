efa336b60f35fd54ac214ce70234744643120022
Keefe Vuong <z5360545@ad.unsw.edu.au>
2021-09-28 01:38:59 +1000

    Fixed up the test cases for list_all

--- tests/channels_test.py
+++ tests/channels_test.py
@@ -9,13 +9,13 @@ from src.error import AccessError
 def test_listall_private_channels():
     clear_v1()
     user_data = auth_register_v1("keefe@gmail.com", "password", "keefe", "vuong")
-    channel_id_1 = channels_create_v1(user_data['auth_user_id'], "channel_1", False)
-    channel_id_2 = channels_create_v1(user_data['auth_user_id'], "channel_2", False)
-    channel_id_3 = channels_create_v1(user_data['auth_user_id'], "channel_3", False)
-    channel_id_4 = channels_create_v1(user_data['auth_user_id'], "channel 4", False)
+    channel_1 = channels_create_v1(user_data['auth_user_id'], "channel_1", False)
+    channel_2 = channels_create_v1(user_data['auth_user_id'], "channel_2", False)
+    channel_3 = channels_create_v1(user_data['auth_user_id'], "channel_3", False)
+    channel_4 = channels_create_v1(user_data['auth_user_id'], "channel_4", False)
 
     channel_info = {
-        "channel_id": [channel_id_1, channel_id_2, channel_id_3, channel_id_4],
+        "channel_id": [channel_1["channel_id"], channel_2["channel_id"], channel_3["channel_id"], channel_4["channel_id"]],
         "name": ["channel_1", "channel_2", "channel_3", "channel_4"],
     }
 
@@ -25,13 +25,13 @@ def test_listall_unauthorised_user_data():
     clear_v1()
     user_data = auth_register_v1("keefe@gmail.com", "password", "keefe", "vuong")
     fake_user_data = 999999
-    channel_id_1 = channels_create_v1(user_data['auth_user_id'], "channel_1", True)
-    channel_id_2 = channels_create_v1(user_data['auth_user_id'], "channel_2", False)
-    channel_id_3 = channels_create_v1(user_data['auth_user_id'], "channel_3", True)
-    channel_id_4 = channels_create_v1(user_data['auth_user_id'], "channel 4", False)
+    channel_1 = channels_create_v1(user_data['auth_user_id'], "channel_1", True)
+    channel_2 = channels_create_v1(user_data['auth_user_id'], "channel_2", False)
+    channel_3 = channels_create_v1(user_data['auth_user_id'], "channel_3", True)
+    channel_4 = channels_create_v1(user_data['auth_user_id'], "channel_4", False)
 
     channel_info = {
-        "channel_id": [channel_id_1, channel_id_2, channel_id_3, channel_id_4],
+        "channel_id": [channel_1["channel_id"], channel_2["channel_id"], channel_3["channel_id"], channel_4["channel_id"]],
         "name": ["channel_1", "channel_2", "channel_3", "channel_4"],
     }
 
@@ -41,13 +41,13 @@ def test_listall_unauthorised_user_data():
 def test_listall_public_channels():
     clear_v1()
     user_data = auth_register_v1("keefe@gmail.com", "password", "keefe", "vuong")
-    channel_id_1 = channels_create_v1(user_data['auth_user_id'], "channel_1", True)
-    channel_id_2 = channels_create_v1(user_data['auth_user_id'], "channel_2", True)
-    channel_id_3 = channels_create_v1(user_data['auth_user_id'], "channel_3", True)
-    channel_id_4 = channels_create_v1(user_data['auth_user_id'], "channel 4", True)
+    channel_1 = channels_create_v1(user_data['auth_user_id'], "channel_1", True)
+    channel_2 = channels_create_v1(user_data['auth_user_id'], "channel_2", True)
+    channel_3 = channels_create_v1(user_data['auth_user_id'], "channel_3", True)
+    channel_4 = channels_create_v1(user_data['auth_user_id'], "channel_4", True)
 
     channel_info = {
-        "channel_id": [channel_id_1, channel_id_2, channel_id_3, channel_id_4],
+        "channel_id": [channel_1["channel_id"], channel_2["channel_id"], channel_3["channel_id"], channel_4["channel_id"]],
         "name": ["channel_1", "channel_2", "channel_3", "channel_4"],
     }
 
@@ -56,13 +56,13 @@ def test_listall_public_channels():
 def test_listall_public_and_private_channels():
     clear_v1()
     user_data = auth_register_v1("keefe@gmail.com", "password", "keefe", "vuong")
-    channel_id_1 = channels_create_v1(user_data['auth_user_id'], "channel_1", True)
-    channel_id_2 = channels_create_v1(user_data['auth_user_id'], "channel_2", True)
-    channel_id_3 = channels_create_v1(user_data['auth_user_id'], "channel_3", False)
-    channel_id_4 = channels_create_v1(user_data['auth_user_id'], "channel_4", False)
+    channel_1 = channels_create_v1(user_data['auth_user_id'], "channel_1", True)
+    channel_2 = channels_create_v1(user_data['auth_user_id'], "channel_2", True)
+    channel_3 = channels_create_v1(user_data['auth_user_id'], "channel_3", False)
+    channel_4 = channels_create_v1(user_data['auth_user_id'], "channel_4", False)
 
     channel_info = {
-        "channel_id": [channel_id_1, channel_id_2, channel_id_3, channel_id_4],
+        "channel_id": [channel_1["channel_id"], channel_2["channel_id"], channel_3["channel_id"], channel_4["channel_id"]],
         "name": ["channel_1", "channel_2", "channel_3", "channel_4"],
     }
 
@@ -72,4 +72,9 @@ def test_listall_no_channels():
     clear_v1()
     user_data = auth_register_v1("keefe@gmail.com", "password", "keefe", "vuong")
 
-    assert channels_listall_v1(user_data['auth_user_id']) == {}
+    channel_info = {
+        "channel_id": [],
+        "name": []
+    }
+
+    assert channels_listall_v1(user_data['auth_user_id']) == channel_info
