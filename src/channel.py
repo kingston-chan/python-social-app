4e1a37aed588771c14129d264c38ade6c214f842
Keefe Vuong <z5360545@ad.unsw.edu.au>
2021-09-28 01:26:05 +1000

    Fixed up the naming convention for storing 'owner_members' and 'all_members'

--- src/channel.py
+++ src/channel.py
@@ -23,8 +23,8 @@ def channel_details_v1(auth_user_id, channel_id):
         raise AccessError("User does not exist")
     
     channel_exists = 0
-    owner_id = None
-    all_members_id = None
+    owner_ids = None
+    all_members_ids = None
     for channel in channels:
         if channel["id"] == channel_id:
             channel_exists = 1
@@ -32,33 +32,21 @@ def channel_details_v1(auth_user_id, channel_id):
             channel_details["is_public"] = channel["is_public"]
             channel_details["owner_members"] = []
             channel_details["all_members"] = []
-            owner_id = channel["owner_members"]
-            all_members_id = channel["all_members"]
+            owner_ids = channel["owner_members"]
+            all_members_ids = channel["all_members"]
 
     if not channel_exists:
         raise InputError("Channel does not exist")
     
     
     for user_info in users:
-        for owners in owner_id:
+        for owners in owner_ids:
             if user_info["id"] == owners:
-                # user_return_info = {}
-                # user_return_info["email"] = user_info["email"]
-                # user_return_info["name_first"] = user_info["name_first"]
-                # user_return_info["name_last"] = user_info["name_last"]
-                # user_return_info["handle"] = user_info["handle"]
-                # user_return_info["id"] = user_info["id"]
                 user_info_placeholder = user_info
-                user_info_placeholder.pop("password", None)
+                user_info_placeholder.pop("password", None) 
                 channel_details["owner_members"].append(user_info_placeholder)
-        for all_members in all_members_id:
+        for all_members in all_members_ids:
             if user_info["id"] == all_members:
-                # user_return_info = {}
-                # user_return_info["email"] = user_info["email"]
-                # user_return_info["name_first"] = user_info["name_first"]
-                # user_return_info["name_last"] = user_info["name_last"]
-                # user_return_info["handle"] = user_info["handle"]
-                # user_return_info["id"] = user_info["id"]
                 user_info_placeholder = user_info
                 user_info_placeholder.pop("password", None)
                 channel_details["all_members"].append(user_info_placeholder)
