194dea8c829fcb19efbb2ef5eb27935b05b857eb
Joshua Cruzado <z5367218@ad.unsw.edu.au>
2021-09-28 12:26:30 +1000

    Tidied up the syntax and added comments

--- src/channel.py
+++ src/channel.py
@@ -29,48 +29,59 @@ def channel_details_v1(auth_user_id, channel_id):
     }
 
 def channel_messages_v1(auth_user_id, channel_id, start):
-    # checks if channel and start are valid inputs
+    # Checks if channel and start are valid inputs
     channel_valid = False
     start_valid = False
     selected_channel = {}
     store = data_store.get()
     channels = store['channels']
-    for channel in channels:
-        if channel["channel_id"] == channel_id:
-            channel_valid = True
-            if start <= len(channel["messages"]):
-                start_valid = True
-                selected_channel = channel
 
+    # Scans if the channel exists
+    for channel in channels:
+        if channel['channel_id'] == channel_id:
+            channel_valid = True
+            # Checks if the start value is valid within the length of 
+            # messages in the channel.
+            if start <= len(channel['messages']):
+                start_valid = True
+                selected_channel = channel  # Channel is also selected
+
+    # If the channel_id or start value are invalid, then errors are raised.
     if channel_valid is False:
         raise InputError("This channel is not valid.")
     if start_valid is False:
         raise InputError("This start is not valid.")
     
+    # Checks if the authorised user is a member of the channel
     member_valid = False
     for member in selected_channel[all_members]:
-        if member["u_id"] == auth_user_id:
+        if member['u_id'] == auth_user_id:
             member_valid = True
-    
-    if member_valid is False:
-        raise AccessError("User is not authorised.")
 
+    # If the user is not a member, then an error is raised.
+    if member_valid is False:
+        raise AccessError('User is not authorised.')
+
+    # The channel is scanned for its messages.
     index = start
     counter = 0
     messages = selected_channel[messages]
     selected_messages = []
     while index <= len(messages) and counter < 50:
         for message in messages:
-            if message["message_id"] == index:
+            if message['message_id'] == index:
                 selected_messages.append(message)
         index += 1
         counter += 1
 
+    # If the scanner hits the end of the messages, the end is -1
+    # else, the end is the final message index.
     if counter != 50:
         end = -1
     else:
         end = index
 
+    # The selected message, the start and the end values are returned.
     return {
         'messages': selected_messages,
         'start': start,
