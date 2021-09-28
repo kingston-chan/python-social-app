854627b76785e365f5e466a1c3be2bf51b180846
z5362176 <z5362176@cse.unsw.edu.au>
2021-09-28 23:39:16 +1000

    Added some basic tests for channels_list_v1

---
+++ tests/test_channels_list_v1.py
@@ -0,0 +1,58 @@
+import pytest
+
+from src.channels import channels_create_v1, channels_list_v1
+from src.channel import channel_details_v1
+from src.auth import auth_register_v1
+from src.error import InputError, AccessError
+from src.other import clear_v1
+
+@pytest.fixture
+def clear_and_register_user():
+    clear_v1()
+    auth_id = auth_register_v1("random@gmail.com", "123abc!@#", "John", "Smith")
+    return auth_id['auth_user_id']
+
+# Returns the only channels that the user is in
+def test_correct_channels(clear_and_register_user):
+    auth_id = clear_and_register_user
+    channel_id1 = channels_create_v1(auth_id, "channel1", True)
+    auth2 = auth_register_v1("validemail@gmail.com", "123abc!@#", "Jane", "Smith")
+    channels_create_v1(auth2['auth_user_id'], "channel2", True)
+    num_channels = 0
+    channels_list = channels_list_v1(auth_id)
+    for channel in channels_list:
+        num_channels += 1
+        assert channel['channel_id'] == channel_id1
+    assert num_channels == 1
+
+
+# AccessError is thrown when invalid user id is given
+def test_invalid_auth_user_id(clear_and_register_user):
+    auth_id = clear_and_register_user
+    with pytest.raises(AccessError):
+        channels_list_v1(auth_id+1)
+
+# Return values are correct i.e. { channel_id, name }
+def test_return_values(clear_and_register_user):
+    auth_id = clear_and_register_user
+    channel1 = channels_create_v1(auth_id, "channel1", True)
+    channel_list = channels_list_v1(auth_id)
+    assert channel_list[0]['channel_id'] == channel1['channel_id']
+    assert type(channel_list[0]['channel_id']) == "int"
+    assert channel_list[0]['name'] == "channel1"
+    assert type(channel_list[0]['name']) == "str"
+
+# Empty list of channels is returned if user does not belong in any channel
+def test_user_not_in_any_channels(clear_and_register_user):
+    auth_id = clear_and_register_user
+    channel_list = channels_list_v1(auth_id)
+    assert channel_list['channels'] == []
+
+# Returns channels that are private that the user is in
+def test_return_private_channels_of_user(clear_and_register_user):
+    auth_id = clear_and_register_user
+    channel1 = channels_create_v1(auth_id, "channel1", False)
+    channel_list = channels_list_v1(auth_id)
+    assert len(channel_list['channels']) == 1
+    assert channel_list['channels'][0]['channel_id'] == channel1['channel_id']
+    assert channel_list['channels'][0]['name'] == "channel1"
