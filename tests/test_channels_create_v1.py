18c54624f295851afaf3980934d687a7b9b316d9
z5362176 <z5362176@cse.unsw.edu.au>
2021-09-30 09:52:39 +1000

    Clearly defined module and integration tests

--- tests/test_channels_create_v1.py
+++ tests/test_channels_create_v1.py
@@ -12,6 +12,39 @@ def clear_and_register_user():
     auth_id = auth_register_v1("random@gmail.com", "123abc!@#", "John", "Smith")
     return auth_id['auth_user_id']
 
+                #============== Module tests ==============#
+
+# InputError when: length of name is less than 1 or more than 20 characters
+def test_invalid_channel_name(clear_and_register_user):
+    auth_id = clear_and_register_user
+    with pytest.raises(InputError):
+        channels_create_v1(auth_id, "", True)
+        channels_create_v1(auth_id, "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa", True)
+
+# Cannot create a channel with same name
+def test_channel_same_name(clear_and_register_user):
+    auth_id = clear_and_register_user
+    channels_create_v1(auth_id, "channel1", True)
+    with pytest.raises(InputError):
+        channels_create_v1(auth_id, "Channel1", True)
+
+# Auth_user_id must be valid, user must exist
+def test_invalid_auth_user_id(clear_and_register_user):
+    auth_id = clear_and_register_user
+    with pytest.raises(AccessError):
+        channels_create_v1(auth_id+1, "channel2", False)
+
+# Removes trailing/leading whitespace
+def test_whitespace(clear_and_register_user):
+    auth_id = clear_and_register_user
+    channels_create_v1(auth_id, "      channel1        ", True)
+    with pytest.raises(InputError):
+        channels_create_v1(auth_id, " chaNNel1     ", False)
+    with pytest.raises(InputError):
+        channels_create_v1(auth_id, "                      ", False)
+
+                #=========== Integration tests ===========#
+
 # Channels_create_v1 returns correct channel_id
 def test_returns_channel_id(clear_and_register_user):
     auth_id = clear_and_register_user  
@@ -46,36 +79,6 @@ def test_returns_channel_id(clear_and_register_user):
 #     assert auth_id['auth_user_id'] == channel_details['all_members'][0]['u_id']
 #     assert type(channel_details["owner_members"]) is list
 #     assert type(channel_details["all_members"]) is list
-
-# InputError when: length of name is less than 1 or more than 20 characters
-def test_invalid_channel_name(clear_and_register_user):
-    auth_id = clear_and_register_user
-    with pytest.raises(InputError):
-        channels_create_v1(auth_id, "", True)
-        channels_create_v1(auth_id, "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa", True)
-
-# Cannot create a channel with same name
-def test_channel_same_name(clear_and_register_user):
-    auth_id = clear_and_register_user
-    channels_create_v1(auth_id, "channel1", True)
-    with pytest.raises(InputError):
-        channels_create_v1(auth_id, "Channel1", True)
-
-# Auth_user_id must be valid, user must exist
-def test_invalid_auth_user_id(clear_and_register_user):
-    auth_id = clear_and_register_user
-    with pytest.raises(AccessError):
-        channels_create_v1(auth_id+1, "channel2", False)
-
-# Removes trailing/leading whitespace
-def test_whitespace(clear_and_register_user):
-    auth_id = clear_and_register_user
-    channels_create_v1(auth_id, "      channel1        ", True)
-    with pytest.raises(InputError):
-        channels_create_v1(auth_id, " chaNNel1     ", False)
-    with pytest.raises(InputError):
-        channels_create_v1(auth_id, "                      ", False)
-
 
 # Channel stores messages
 # def test_channel_members(clear_and_register_user):
