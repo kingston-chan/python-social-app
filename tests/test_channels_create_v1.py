8dcd1e463c25962aace37eb34d905453c1e0bee0
Keefe Vuong <z5360545@ad.unsw.edu.au>
2021-09-29 13:35:25 +1000

    Uncommented a file import that was failing previously

--- tests/test_channels_create_v1.py
+++ tests/test_channels_create_v1.py
@@ -1,7 +1,7 @@
 import pytest
 
 from src.channels import channels_create_v1, channels_list_v1
-#from src.channel import channel_details_v1
+from src.channel import channel_details_v1
 from src.auth import auth_register_v1
 from src.error import InputError, AccessError
 from src.other import clear_v1
