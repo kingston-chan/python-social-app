399caeca975f83c7721bbe4ba80389177aff11d6
z5362176 <z5362176@cse.unsw.edu.au>
2021-10-11 13:52:59 +1100

    Create stub functions for all the features

--- src/server.py
+++ src/server.py
@@ -30,14 +30,185 @@ APP.register_error_handler(Exception, defaultHandler)
 #### NO NEED TO MODIFY ABOVE THIS POINT, EXCEPT IMPORTS
 
 # Example
-@APP.route("/echo", methods=['GET'])
-def echo():
-    data = request.args.get('data')
-    if data == 'echo':
-   	    raise InputError(description='Cannot echo "echo"')
-    return dumps({
-        'data': data
-    })
+# @APP.route("/echo", methods=['GET'])
+# def echo():
+#     data = request.args.get('data')
+#     if data == 'echo':
+#    	    raise InputError(description='Cannot echo "echo"')
+#     return dumps({
+#         'data': data
+#     })
+
+#====== auth.py =====#
+
+# auth/login/v2
+@APP.route("/auth/login/v2", methods=['POST'])
+def auth_login():
+    return {}
+
+# auth/register/v2
+@APP.route("/auth/register/v2", methods=['POST'])
+def auth_register():
+    return {}
+
+# auth/logout/v1
+@APP.route("/auth/logout/v1", methods=['POST'])
+def auth_logout():
+    return {}
+
+#====== channels.py =====#
+
+# channels/create/v2
+@APP.route("/channels/create/v2", methods=['POST'])
+def channels_create():
+    return {}
+
+# channels/list/v2
+@APP.route("/channels/list/v2", methods=['GET'])
+def channels_list():
+    return {}
+
+# channels/listall/v2
+@APP.route("/channels/listall/v2", methods=['GET'])
+def channels_listall():
+    return {}
+
+#====== channel.py =====#
+
+# channel/details/v2
+@APP.route("/channel/details/v2", methods=['GET'])
+def channel_details():
+    return {}
+
+# channel/join/v2
+@APP.route("/channel/join/v2", methods=['POST'])
+def channel_join():
+    return {}
+
+# channel/invite/v2
+@APP.route("/channel/invite/v2", methods=['POST'])
+def channel_invite():
+    return {}
+
+# channel/messages/v2
+@APP.route("/channel/messages/v2", methods=['GET'])
+def channel_messages():
+    return {}
+
+# channel/leave/v1
+@APP.route("/channel/leave/v1", methods=['POST'])
+def channel_leave():
+    return {}
+
+# channel/addowner/v1
+@APP.route("/channel/addowner/v1", methods=['POST'])
+def channel_addowner():
+    return {}
+
+# channel/removeowner/v1
+@APP.route("/channel/removeowner/v1", methods=['POST'])
+def channel_removeowner():
+    return {}
+
+#====== message.py =====#
+
+# message/send/v1
+@APP.route("/message/send/v1", methods=['POST'])
+def message_send():
+    return {}
+
+# message/edit/v1
+@APP.route("/message/edit/v1", methods=['PUT'])
+def message_edit():
+    return {}
+
+# message/remove/v1
+@APP.route("/message/remove/v1", methods=['DELETE'])
+def message_remove():
+    return {}
+
+# message/senddm/v1
+@APP.route("/message/senddm/v1", methods=['POST'])
+def message_senddm():
+    return {}
+
+#====== dm.py =====#
+
+# dm/create/v1
+@APP.route("/dm/create/v1", methods=['POST'])
+def dm_create():
+    return {}
+
+# dm/list/v1
+@APP.route("/dm/list/v1", methods=['GET'])
+def dm_list():
+    return {}
+
+# dm/remove/v1
+@APP.route("/dm/remove/v1", methods=['DELETE'])
+def dm_remove():
+    return {}
+
+# dm/details/v1
+@APP.route("/dm/details/v1", methods=['GET'])
+def login():
+    return {}
+
+# dm/leave/v1
+@APP.route("/dm/leave/v1", methods=['POST'])
+def dm_leave():
+    return {}
+
+# dm/messages/v1
+@APP.route("/dm/messages/v1", methods=['GET'])
+def login():
+    return {}
+
+#===== users.py =====#
+
+# users/all/v1
+@APP.route("/users/all/v1", methods=['GET'])
+def users_all():
+    return {}
+
+#===== user.py =====#
+
+# user/profile/v1
+@APP.route("/user/profile/v1", methods=['GET'])
+def user_profile():
+    return {}
+
+# user/profile/setname/v1
+@APP.route("/user/profile/setname/v1", methods=['PUT'])
+def user_profile_setname():
+    return {}
+
+# user/profile/setemail/v1
+@APP.route("/user/profile/setemail/v1", methods=['PUT'])
+def user_profile_setemail():
+    return {}
+
+# user/profile/sethandle/v1
+@APP.route("/user/profile/sethandle/v1", methods=['PUT'])
+def user_profile_sethandle():
+    return {}
+
+#===== admin.py =====#
+
+# admin/user/remove/v1
+@APP.route("/admin/user/remove/v1", methods=['DELETE'])
+def admin_user_remove():
+    return {}
+
+# admin/userpermission/change/v1
+@APP.route("/admin/userpermission/change/v1", methods=['POST'])
+def admin_userpermission_change():
+    return {}
+
+# clear/v1
+@APP.route("/clear/v1", methods=['DELETE'])
+def clear_v1():
+    return {}
 
 #### NO NEED TO MODIFY BELOW THIS POINT
 
