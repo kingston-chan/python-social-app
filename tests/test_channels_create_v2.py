1d03c6828286bd3dd7486bf322253f54eb2526af
z5362176 <z5362176@cse.unsw.edu.au>
2021-10-14 16:06:06 +1100

    Added helper functions to create and list channels

--- tests/test_channels_create_v2.py
+++ tests/test_channels_create_v2.py
@@ -17,25 +17,31 @@ def clear_and_register():
     response_data = response.json()
     return response_data['token']
 
+# ==== Helper functions ==== #
+
+def create_channel(token, name, is_public):
+    channel_info = {
+        "token": token, 
+        "name": name, 
+        "is_public": is_public
+    }
+
+    return requests.post(f"{BASE_URL}/channels/create/v2", json=channel_info)
+
+def list_channels(token):
+    return requests.get(f"{BASE_URL}/channels/list/v2", params={ "token": token })
+
 # ==== Tests with correct input ==== #
 
 # Creates a channel successfully
 def test_can_create_channel(clear_and_register):
-    user_token = clear_and_register
-
-    channel_info = {
-        "token": user_token, 
-        "name": "channel1", 
-        "is_public": "True"
-    }
-
-    response = requests.post(f"{BASE_URL}/channels/create/v2", json=channel_info)
+    response = create_channel(clear_and_register, "channel1", "True")
     assert response.status_code == 200
     
     response_data = response.json()
     channel1_id = response_data['channel_id']
 
-    response = requests.get(f"{BASE_URL}/channels/list/v2", params={ "token": user_token })
+    response = list_channels(clear_and_register)
     response_data = response.json()
     
     assert response_data["channels"][0]["channel_id"] == channel1_id
@@ -47,57 +53,29 @@ def test_can_create_channel(clear_and_register):
 # Invalid token
 def test_invalid_token():
     requests.delete(f"{BASE_URL}/clear/v1")
-    channel_info = {
-        "token": "invalidtoken", 
-        "name": "channel1", 
-        "is_public": "True"
-    }
-
-    response = requests.post(f"{BASE_URL}/channels/create/v2", json=channel_info)
-    assert response.status_code == 400
-
-
+    response = create_channel("invalidtoken", "channel1", "True")
+    assert response.status_code == 403
 
 # Invalid name (less than 1 character, more than 20 characters)
 def test_invalid_name(clear_and_register):
-    user_token = clear_and_register
 
-    channel_info = {
-        "token": user_token, 
-        "name": "", 
-        "is_public": "True"
-    }
-
-    response = requests.post(f"{BASE_URL}/channels/create/v2", json=channel_info)
+    response = create_channel(clear_and_register, "", "True")
     assert response.status_code == 400
 
-    channel_info["name"] = "morethantwentycharacters"
-    response = requests.post(f"{BASE_URL}/channels/create/v2", json=channel_info)
+    response = create_channel(clear_and_register, "morethantwentycharacters", "True")
     assert response.status_code == 400
 
-    channel_info["name"] = "                    "
-    response = requests.post(f"{BASE_URL}/channels/create/v2", json=channel_info)
+    response = create_channel(clear_and_register, "                    ", "True")
     assert response.status_code == 400
-
 
 # Channel name already exists
 def test_channel_name_already_exists(clear_and_register):
-    user_token = clear_and_register
-
-    channel_info = {
-        "token": user_token, 
-        "name": "channel1", 
-        "is_public": "True"
-    }
-
-    requests.post(f"{BASE_URL}/channels/create/v2", json=channel_info)
-    response = requests.post(f"{BASE_URL}/channels/create/v2", json=channel_info)
+    create_channel(clear_and_register, "channel1", "True")
+    response = create_channel(clear_and_register, "channel1", "True")
     assert response.status_code == 400
-    
-    channel_info["name"] = "CHANNEL1"
-    response = requests.post(f"{BASE_URL}/channels/create/v2", json=channel_info)
+
+    response = create_channel(clear_and_register, "CHANNEL1", "True")
     assert response.status_code == 400
-    
-    channel_info["name"] = "     CHannEL1     "
-    response = requests.post(f"{BASE_URL}/channels/create/v2", json=channel_info)
+
+    response = create_channel(clear_and_register, "     CHannEL1     ", "True")
     assert response.status_code == 400
