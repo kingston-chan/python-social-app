ce644d81d2eea3838d91884414ff0107c0a86758
z5362176 <z5362176@cse.unsw.edu.au>
2021-10-14 16:20:30 +1100

    Added helper function to register users and to list and create channels

--- tests/test_channels_list_v2.py
+++ tests/test_channels_list_v2.py
@@ -2,82 +2,62 @@ import requests, pytest
 
 BASE_URL = 'http://127.0.0.1:8080'
 
+
+# ==== Helper functions ==== #
+
+# Registers a user
+def register_user(email, password, first_name, last_name):
+    user_info = {
+        "email": email, 
+        "password": password, 
+        "name_first": first_name, 
+        "name_last": last_name
+    }
+    return requests.post(f"{BASE_URL}/auth/register/v2", json=user_info).json()
+
+
+# Creates channel and returns created channel_id
+def create_channel(token, name, is_public):
+    channel_info = {
+        "token": token, 
+        "name": name, 
+        "is_public": is_public
+    }
+
+    return requests.post(f"{BASE_URL}/channels/create/v2", json=channel_info).json()["channel_id"]
+
+# Lists all channels user is in
+def list_channels(token):
+    return requests.get(f"{BASE_URL}/channels/list/v2", params={ "token": token })
+
 @pytest.fixture
 def clear_and_register():
     requests.delete(f"{BASE_URL}/clear/v1")
+    return register_user("random@gmail.com", "123abc!@#", "John", "Smith")['token']
 
-    user_info = {
-        "email": "random@gmail.com", 
-        "password": "123abc!@#", 
-        "name_first": "John", 
-        "name_last": "Smith"
-    }
-
-    response = requests.post(f"{BASE_URL}/auth/register/v2", json=user_info)
-    response_data = response.json()
-    return response_data['token']
 
 # ==== Tests with correct input ==== #
 
 # List channels authorised user is in
 def test_user_list_channel(clear_and_register):
-    user_token = clear_and_register
+    channel1_id = create_channel(clear_and_register, "channel1", "True")
+    channel2_id = create_channel(clear_and_register, "channel2", "True")
 
-    channel_info = {
-        "token": user_token, 
-        "name": "channel1", 
-        "is_public": "True"
-    }
-
-    response = requests.post(f"{BASE_URL}/channels/create/v2", json=channel_info)
-    response_data = response.json()
-    channel1_id = response_data["channel_id"]
-
-    channel_info["name"] = "channel2"
-    response = requests.post(f"{BASE_URL}/channels/create/v2", json=channel_info)
-    response_data = response.json()
-    channel2_id = response_data["channel_id"]
-
-    response = requests.get(f"{BASE_URL}/channels/list/v2", params={ "token": user_token })
-    assert response.status_code == 200
-
-    response_data = response.json()
+    response_data = list_channels(clear_and_register).json()
+    
     assert len(response_data["channels"]) == 2
     assert response_data["channels"][0]["channel_id"] == channel1_id
     assert response_data["channels"][1]["channel_id"] == channel2_id
 
 # List only channels authorised user is in, including private ones
 def test_only_list_authorised_user_channels(clear_and_register):
-    user_token1 = clear_and_register
+    user_token2 = register_user("random2@gmail.com", "123abc!@#", "Bob", "Smith")['token']
 
-    user_info2 = {
-        "email": "random2@gmail.com", 
-        "password": "123abc!@#", 
-        "name_first": "Bob", 
-        "name_last": "Smith"
-    }
+    channel1_id = create_channel(clear_and_register, "channel1", "False")
 
-    response = requests.post(f"{BASE_URL}/auth/register/v2", json=user_info2)
-    response_data = response.json()
-    user_token2 = response_data['token']
+    create_channel(user_token2, "Channel2", "True")
 
-    channel_info = {
-        "token": user_token1, 
-        "name": "channel1", 
-        "is_public": "False"
-    }
-
-    response = requests.post(f"{BASE_URL}/channels/create/v2", json=channel_info)
-    response_data = response.json()
-    channel1_id = response_data["channel_id"]
-
-    channel_info["token"] = user_token2
-    channel_info["name"] = "channel2"
-
-    requests.post(f"{BASE_URL}/channels/create/v2", json=channel_info)
-
-    response = requests.get(f"{BASE_URL}/channels/list/v2", params={ "token": user_token1 })
-    response_data = response.json()
+    response_data = list_channels(clear_and_register).json()
     assert len(response_data["channels"]) == 1
     assert response_data["channels"][0]["channel_id"] == channel1_id
 
@@ -87,7 +67,7 @@ def test_only_list_authorised_user_channels(clear_and_register):
 # Invalid token
 def test_invalid_token():
     requests.delete(f"{BASE_URL}/clear/v1")
-    response = requests.get(f"{BASE_URL}/channels/list/v2", params={ "token": "invalidtoken" })
+    response = list_channels("invalidtoken")
     assert response.status_code == 400
 
 
