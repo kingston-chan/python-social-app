56939d78b7346f95409d5b7b5e74530ce38b604e
z5362176 <z5362176@cse.unsw.edu.au>
2021-10-16 18:40:21 +1100

    Replaced helper functions with functions from route_helpers

--- tests/test_channel_leave_v1.py
+++ tests/test_channel_leave_v1.py
@@ -1,82 +1,42 @@
-import requests, pytest
-
-BASE_URL = 'http://127.0.0.1:8080'
-
-# ==== Helper functions ==== #
-
-# Registers a user
-def register_user(email, password, first_name, last_name):
-    user_info = {
-        "email": email, 
-        "password": password, 
-        "name_first": first_name, 
-        "name_last": last_name
-    }
-
-    return requests.post(f"{BASE_URL}/auth/register/v2", json=user_info).json()
-
-# Creates channel and returns created channel_id
-def create_channel(token, name, is_public):
-    channel_info = {
-        "token": token, 
-        "name": name, 
-        "is_public": is_public
-    }
-
-    return requests.post(f"{BASE_URL}/channels/create/v2", json=channel_info).json()["channel_id"]
-
-# Lists all channels user is in
-def list_channels(token):
-    return requests.get(f"{BASE_URL}/channels/list/v2", params={ "token": token }).json()
-
-# Joins the channel of given channel_id
-def join_channel(token, channel_id):
-    return requests.post(f"{BASE_URL}/channel/join/v2", json={ "token": token, "channel_id": channel_id })
-
-# Lists all existing channels
-def list_all_channels(token):
-    return requests.get(f"{BASE_URL}/channels/list/v2", params={ "token": token }).json()
-
-# Leave the given channel
-def leave_channel(token, channel_id):
-    return requests.post(f"{BASE_URL}/channels/list/v2", json={ "token": token, "channel_id": channel_id })
+import pytest
+from tests.route_helpers import auth_register, channel_leave, channels_create, channels_list, channels_listall, clear 
 
 @pytest.fixture
 def clear_and_register():
-    requests.delete(f"{BASE_URL}/clear/v1")
-    return register_user("random@gmail.com", "123abc!@#", "John", "Smith")
+    clear()
+    return auth_register("random@gmail.com", "123abc!@#", "John", "Smith").json()["token"]
 
 # ==== Tests with correct input ==== #
 
 # Owner is able to leave the channel, but channel still exists, even with no members
 def test_owner_leaves_channel_still_exists(clear_and_register):
-    channel1_id = create_channel(clear_and_register, "channel1", "True")
+    channel1_id = channels_create(clear_and_register, "channel1", "True").json()["channel_id"]
     
-    leave_channel(clear_and_register, channel1_id)
+    channel_leave(clear_and_register, channel1_id)
     
-    response_data = list_channels(clear_and_register)
+    response_data = channels_list(clear_and_register).json()
     assert len(response_data["channels"]) == 0
     
-    response_data = list_all_channels(clear_and_register)
+    response_data = channels_listall(clear_and_register).json()
     assert len(response_data["channels"]) == 1
 
 # ==== Tests with incorrect/invalid input ==== #
 
 # Invalid token, but valid channel id
 def test_invalid_token(clear_and_register):
-    channel1_id = create_channel(clear_and_register, "channel1", "True")
-    response = leave_channel("invalidtoken", channel1_id)
+    channel1_id = channels_create(clear_and_register, "channel1", "True")
+    response = channel_leave("invalidtoken", channel1_id)
     assert response.status_code == 403
 
 # Valid token but invalid channel id
 def test_invalid_channel_id(clear_and_register):
-    response = leave_channel(clear_and_register, "invalidtoken")
+    response = channel_leave(clear_and_register, 1)
     assert response == 400
 
 # Valid token and channel_id but token's payload contains unauthorised id
 def test_unauthorised_id(clear_and_register):
-    channel1_id = create_channel(clear_and_register, "channel1", "True")
-    user2_token = register_user("random2@gmail.com", "123abc!@#", "Bob", "Smith")["token"]
-    response = leave_channel(user2_token, channel1_id)
+    channel1_id = channels_create(clear_and_register, "channel1", "True")
+    user2_token = auth_register("random2@gmail.com", "123abc!@#", "Bob", "Smith").json()["token"]
+    response = channel_leave(user2_token, channel1_id)
     assert response.status_code == 403
 
