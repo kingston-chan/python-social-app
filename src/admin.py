9b4409bb43e932f9387d64cf75817717d9efba0e
z5362176 <z5362176@cse.unsw.edu.au>
2021-10-17 13:47:23 +1100

    Written code for admin_userpermission_change function and added it to the route

---
+++ src/admin.py
@@ -0,0 +1,77 @@
+"""
+Functions to:
+- Remove user from streams, removes them from all channels and dms
+- Change user's permission from member to global owner and global owner to member
+"""
+
+from src.data_store import data_store
+from src.error import InputError, AccessError
+
+def admin_userpermission_change_v1(auth_user_id, u_id, permission_id):
+    """
+    Given a user by their user ID, set their permissions to new permissions described by permission_id.
+
+    Arguments:
+        auth_user_id (int) - id of the authenticated user
+        u_id (int) - id of the user being its permission changed
+        permission_id (int) - id of the permission being assigned to the user, i.e. 1 for global owner
+                              and 2 for member  
+
+    Exceptions:
+        InputError occurs when:
+            - u_id does not refer to a valid user
+            - u_id refers to user who is the only global owner and they are being demoted to a member
+            - permission_id is invalid
+        AccessError occurs when:
+            - auth_user_id is not a global owner
+    
+    Return Value
+        Returns an empty dictionary on successful user permission change
+    """
+    store = data_store.get()
+    users = store["users"]
+    
+    # Exceptions
+    global_owners = list(filter(lambda user: (user["permission"] == 1), users))
+    # Not a global owner
+    if len(list(filter(lambda owner: (owner["u_id"] == auth_user_id), global_owners))) != 1:
+        raise AccessError("Not global owner")
+    user_being_changed = list(filter(lambda user: (user["u_id"] == u_id), users))
+    # u_id does not belong to anyone on streams
+    if len(user_being_changed) != 1:
+        raise InputError("Invalid User")
+    # u_id belongs to a removed user of streams
+    if user_being_changed[0]["email"] is None and user_being_changed[0]["handle"] is None:
+        raise InputError("Remove User")
+    # u_id refers to only global owner
+    if len(global_owners) == 1 and user_being_changed[0]["permission"] == 1:
+        raise InputError("Only global owner left, cannot remove")
+    # Invalid permission id
+    if 1 > permission_id > 2:
+        raise InputError("Invalid permission id")
+
+    channels = store["channels"]
+    if permission_id == 1:
+        user_being_changed[0]["permission"] = 1
+        new_channels = list(filter(lambda channel: (u_id in channel["all_members"]),
+            filter(lambda channel: (not u_id in channel["owner_permissions"]), 
+                map(lambda channel: channel["owner_permissions"].append(u_id), channels)
+            ))
+        )
+    else:
+        user_being_changed[0]["permission"] = 2
+        new_channels = list(filter(lambda channel: (u_id in channel["all_members"]),
+            filter(lambda channel: (u_id in channel["owner_permissions"] and not u_id in ["owner_members"]), 
+                map(lambda channel: channel["owner_permissions"].remove(u_id), channels)
+            ))
+        )
+    
+    store["channels"] = new_channels
+    data_store.set(store)
+    return {}
+
+
+
+    
+
+    
