dac8ab0c6ade0da4214004deac6d0d52f49afd7f
z5362176 <z5362176@cse.unsw.edu.au>
2021-10-18 20:01:42 +1100

    Uncommented out tests and fixed style by using route_helpers

--- tests/test_users_all_v1.py
+++ tests/test_users_all_v1.py
@@ -1,62 +1,71 @@
-import pytest, requests
-from src.config import url
+import pytest
+import tests.route_helpers as rh
 
-# ==== Helper function ==== #
-def register_user(email, password, first_name, last_name):
-    user_info = {
-        "email": email, 
-        "password": password, 
-        "name_first": first_name, 
-        "name_last": last_name
-    }
-    return requests.post(f"{url}/auth/register/v2", json=user_info).json()
+@pytest.fixture
+def clear_and_register():
+    rh.clear()
+    return rh.auth_register("random@gmail.com", "123abc!@#", "John", "Smith").json()["token"]
 
 # ==== Tests with correct input ==== #
 
 # Lists all users given valid token
-def test_list_all_users():
-    requests.delete(f"{url}/clear/v1")
-    
-    response_data = register_user("random@gmail.com", "123abc!@#", "John", "Smith")
-    user_token1 = response_data["token"]
-    user1_id = response_data["auth_user_id"]
-    response_data = register_user("random1@gmail.com", "123abc!@#", "Bob", "Smith")
-    user2_id = response_data["auth_user_id"]
+def test_list_all_users(clear_and_register):
 
-    response_data = register_user("random2@gmail.com", "123abc!@#", "Dan", "Smith")
-    user3_id = response_data["auth_user_id"] 
+    user1_id = rh.auth_login("random@gmail.com", "123abc!@#").json()["auth_user_id"]
+    user2_id = rh.auth_register("random1@gmail.com", "123abc!@#", "Bob", "Smith").json()["auth_user_id"]
+    user3_id = rh.auth_register("random2@gmail.com", "123abc!@#", "Dan", "Smith").json()["auth_user_id"]
 
-    response = requests.get(f"{url}/users/all/v1", params={ "token": user_token1 })
+    response = rh.users_all(clear_and_register)
     assert response.status_code == 200
 
-    response_data = response.json()
-    print(response_data)
-    assert len(response_data["users"]) == 3
+    all_users = response.json()["users"]
 
-    assert response_data["users"][0]["u_id"] == user1_id
-    assert response_data["users"][1]["u_id"] == user2_id
-    assert response_data["users"][2]["u_id"] == user3_id
+    assert len(all_users) == 3
 
-    assert response_data["users"][0]["email"] == "random@gmail.com"
-    assert response_data["users"][1]["email"] == "random1@gmail.com"
-    assert response_data["users"][2]["email"] == "random2@gmail.com"
+    assert all_users[0]["u_id"] == user1_id
+    assert all_users[1]["u_id"] == user2_id
+    assert all_users[2]["u_id"] == user3_id
 
-    assert response_data["users"][0]["name_first"] == "John"
-    assert response_data["users"][1]["name_first"] == "Bob"
-    assert response_data["users"][2]["name_first"] == "Dan"
+    assert all_users[0]["email"] == "random@gmail.com"
+    assert all_users[1]["email"] == "random1@gmail.com"
+    assert all_users[2]["email"] == "random2@gmail.com"
+
+    assert all_users[0]["name_first"] == "John"
+    assert all_users[1]["name_first"] == "Bob"
+    assert all_users[2]["name_first"] == "Dan"
+
+# Lists all valid users, i.e. not removed
+def test_list_valid_users(clear_and_register):
+    user1_id = rh.auth_login("random@gmail.com", "123abc!@#").json()["auth_user_id"]
+    user2_id = rh.auth_register("random1@gmail.com", "123abc!@#", "Bob", "Smith").json()["auth_user_id"]
+    user3_id = rh.auth_register("random2@gmail.com", "123abc!@#", "Dan", "Smith").json()["auth_user_id"]
+
+    rh.admin_user_remove(clear_and_register, user2_id)
+
+    all_users = rh.users_all(clear_and_register).json()["users"]
+
+    assert len(all_users) == 2
+
+    assert all_users[0]["u_id"] == user1_id
+    assert all_users[1]["u_id"] == user3_id
+
+    assert all_users[0]["email"] == "random@gmail.com"
+    assert all_users[1]["email"] == "random2@gmail.com"
+
+    assert all_users[0]["name_first"] == "John"
+    assert all_users[1]["name_first"] == "Dan"
+
 
 # ==== Tests with incorrect/invalid input ==== #
 
 # Invalid token
 def test_invalid_token():
-    requests.delete(f"{url}/clear/v1")
-    response = requests.get(f"{url}/users/all/v1", params={ "token": "invalidtoken" })
+    rh.clear()
+    response = rh.users_all("invalidtoken")
     assert response.status_code == 403
 
 # Invalid session
-# def test_invalid_session():
-#     requests.delete(f"{url}/clear/v1")
-#     user_token = register_user("random@gmail.com", "123abc!@#", "John", "Smith")["token"]
-#     requests.post(f"{url}/auth/logout/v1", json={ "token": user_token })
-#     response = requests.get(f"{url}/users/all/v1", params={ "token": user_token })
-#     assert response.status_code == 403
+def test_invalid_session(clear_and_register):
+    rh.auth_logout(clear_and_register)
+    response = rh.users_all(clear_and_register)
+    assert response.status_code == 403
