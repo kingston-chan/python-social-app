41de0731da44609dfcee9f23b44fe79d998032b6
Abbas Zaidi <abbas.zaidi1@student.unsw.edu.au>
2021-10-18 11:19:59 +1100

    written two intial tests for invalid token and invalid u_ids

---
+++ tests/test_dm_create_v1.py
@@ -0,0 +1,25 @@
+import requests
+import pytest
+from src.config import url
+
+BASE_URL = url
+
+#400 is input error
+#403 is access error
+
+def invlaid_user_ids():
+    requests.delete(f"{url}/clear/v1")
+
+    new_user = {"email" : "fakeguy@gmail.com" , "password": "fake12345","name_first" : "faker", "name_last" : "is_a_faker" }
+
+    response = requests.post(f"{url}/auth/register/v2", json=new_user) 
+    response_data = response.json()
+    new_user_token = response_data["token"]
+
+    response = requests.post(f"{url}/dm/create/v1", json={"token" : new_user_token "u_ids" : [1]})
+    assert response.status_code == 400   
+def invlaid_token():
+    requests.delete(f"{url}/clear/v1")
+
+    response = requests.post(f"{url}/dm/create/v1", json={"token" : new_user_token "u_ids" : [1]})
+    assert response.status_code == 403 
