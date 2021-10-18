e71380475f08966ef6df2c09b1ac25e61967440e
z5362176 <z5362176@cse.unsw.edu.au>
2021-10-18 16:17:40 +1100

    Added admin user remove function to the corresponding route

--- src/server.py
+++ src/server.py
@@ -20,7 +20,7 @@ from src.channels import channels_listall_v1
 from src.channel import channel_join_v1, channel_leave_v1, channel_messages_v1, channel_invite_v1, channel_details_v1, channel_addowner_v1, channel_removeowner_v1
 from src.user import list_all_users, user_profile_v1
 from src.message import message_send_v1, message_edit_v1, message_remove_v1
-from src.admin import admin_userpermission_change_v1
+from src.admin import admin_user_remove_v1, admin_userpermission_change_v1
 
 HASHCODE = "LKJNJLKOIHBOJHGIUFUTYRDUTRDSRESYTRDYOJJHBIUYTF"
 
@@ -375,7 +375,11 @@ def user_profile_sethandle():
 # admin/user/remove/v1
 @APP.route("/admin/user/remove/v1", methods=['DELETE'])
 def admin_user_remove():
-    return {}
+    data = request.get_json()
+    auth_user_id = check_valid_token_and_session(data["token"])
+    admin_user_remove_v1(auth_user_id, data["u_id"])
+    save()
+    return dumps({})
 
 # admin/userpermission/change/v1
 @APP.route("/admin/userpermission/change/v1", methods=['POST'])
