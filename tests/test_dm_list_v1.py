f3c8f5a95c7ea557c49627dc65c9b2419394220e
Abbas Zaidi <abbas.zaidi1@student.unsw.edu.au>
2021-10-19 01:31:38 +1100

    written inital test for successful case and invalid token test

---
+++ tests/test_dm_list_v1.py
@@ -0,0 +1,51 @@
+import requests
+import pytest
+from src.config import url
+
+BASE_URL = url
+
+#400 is input error
+#403 is access error
+
+def test_invlaid_token():
+    requests.delete(f"{url}/clear/v1")
+    response = requests.get(f"{url}/dm/list/v1", params={"token" : 1 , "u_ids" : [1]})
+    assert response.status_code == 403 
+
+
+def test_succesful_case1():
+    requests.delete(f"{url}/clear/v1")
+
+    new_user = {"email" : "fakeguy@gmail.com" , "password": "fake12345","name_first" : "faker", "name_last" : "is_a_faker" }
+
+    response = requests.post(f"{url}/auth/register/v2", json=new_user) 
+    response_data = response.json()
+    new_user_token = response_data["token"]
+    owner_uid = response_data["auth_user_id"]
+
+    new_user = {"email" : "fakeguy1@gmail.com" , "password": "fake123456","name_first" : "faker1", "name_last" : "is_a_faker1" }
+
+    response = requests.post(f"{url}/auth/register/v2", json=new_user) 
+    response_data = response.json()
+    input_token = response_data["token"]
+    u_id = response_data["auth_user_id"]
+
+    response = requests.post(f"{BASE_URL}/dm/create/v1", json={ "token" : new_user_token, "u_ids" : [u_id] })
+    response_data = response.json()
+    dm_id = response_data["dm_id"]
+
+    list_of_dicts = requests.get(f"{url}/dm/list/v1", params={"token" : input_token })
+    #list_of_dicts = {dm_id , name}
+    #dm_details(dm_id) = {name, members}
+
+    i = True
+
+    for dict in list_of_dicts:
+        list_of_members = requests.get(f"{BASE_URL}/dm/details/v1", params=dict["dm_id"])
+        if u_id not in list_of_members["members"]:
+            i = False
+    
+    assert i == True
+
+
+
