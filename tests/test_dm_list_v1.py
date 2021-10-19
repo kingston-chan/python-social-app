a5358e69171cba384c92a67a820a20eb43c18782
Abbas Zaidi <abbas.zaidi1@student.unsw.edu.au>
2021-10-19 15:31:16 +1100

    adding second successful case

--- tests/test_dm_list_v1.py
+++ tests/test_dm_list_v1.py
@@ -36,28 +36,43 @@ def test_succesful_case1():
 
     list_of_dicts = requests.get(f"{url}/dm/list/v1", params={"token" : input_token })
 
-    #list_of_dicts = {dm_id , name}
-    #dm_details(dm_id) = {name, members}
-
-
     list_of_dicts = list_of_dicts.json()
-    print (list_of_dicts)
-    created_dict = list_of_dicts["dms"][0]
-    print(created_dict)
-    created_dm_id = created_dict["dm_id"]
 
+    expected_output = { "dms" : [{'dm_id': 1, 'name': 'faker1isafaker, fakerisafaker1'}]}
     
+    assert list_of_dicts == expected_output
+
+    def test_succesful_case2():
+        requests.delete(f"{url}/clear/v1")
+
+        new_user = {"email" : "fakeguy@gmail.com" , "password": "fake12345","name_first" : "faker", "name_last" : "is_a_faker" }
+        new_user1 = {"email" : "fakeguy1@gmail.com" , "password": "fake123456","name_first" : "faker", "name_last" : "is_a_faker1" }
+        new_user2 = {"email" : "fakeguy2@gmail.com" , "password": "fake123457","name_first" : "faker", "name_last" : "is_a_faker2" }
+
+        response = requests.post(f"{url}/auth/register/v2", json=new_user)
+        u_id = response["auth_user_id"]
+        owner_token = response["token"]
+        response = requests.post(f"{url}/auth/register/v2", json=new_user1)
+        u_id1 = response["auth_user_id"]
+        input_token = response["token"]
+        response = requests.post(f"{url}/auth/register/v2", json=new_user2)
+        u_id2 = response["auth_user_id"]
+
+        response = requests.post(f"{BASE_URL}/dm/create/v1", json={ "token" : owner_token, "u_ids" : [u_id1,u_id2] })
+        response_data = response.json()
+        dm_id = response_data["dm_id"]
+
+        list_of_dicts = requests.get(f"{url}/dm/list/v1", params={"token" : input_token })
+
+        output = list_of_dicts.json()
+
+        expected_output = {"dms" : [{"dm_id" : 1 , 'name': "faker1isafaker , faker1isafaker1 ,fakerisafaker2" }]}
+
+        list_of_dicts = requests.get(f"{url}/dm/list/v1", params={"token" : input_token })
+
+        assert expected_output == list_of_dicts
 
 
-    response = requests.get(f"{BASE_URL}/dm/details/v1", params={"token": input_token,"dm_id" : created_dm_id})
-
-    response_data = response.json()
-    print(response_data)
-    if u_id not in response_data["members"]:
-        i = False
-
-    assert i  == True
-    
 
 
 
