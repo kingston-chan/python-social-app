65fe49e8a31f56b89efbc0d57ce82dd98adf7541
Abbas Zaidi <abbas.zaidi1@student.unsw.edu.au>
2021-10-19 22:31:25 +1100

    added new successful tests

--- tests/test_dm_list_v1.py
+++ tests/test_dm_list_v1.py
@@ -1,6 +1,7 @@
 import requests
 import pytest
 from src.config import url
+from src.auth import auth_logout_v1
 
 BASE_URL = url
 
@@ -23,7 +24,7 @@ def test_succesful_case1():
     new_user_token = response_data["token"]
     owner_uid = response_data["auth_user_id"]
 
-    new_user = {"email" : "fakeguy1@gmail.com" , "password": "fake123456","name_first" : "faker1", "name_last" : "is_a_faker1" }
+    new_user = {"email" : "fakeguy1@gmail.com" , "password": "fake123456","name_first" : "faker", "name_last" : "is_a_faker1" }
 
     response = requests.post(f"{url}/auth/register/v2", json=new_user) 
     response_data = response.json()
@@ -32,45 +33,117 @@ def test_succesful_case1():
 
     response = requests.post(f"{BASE_URL}/dm/create/v1", json={ "token" : new_user_token, "u_ids" : [u_id] })
     response_data = response.json()
-    dm_id = response_data["dm_id"]
 
     list_of_dicts = requests.get(f"{url}/dm/list/v1", params={"token" : input_token })
 
     list_of_dicts = list_of_dicts.json()
 
-    expected_output = { "dms" : [{'dm_id': 1, 'name': 'faker1isafaker, fakerisafaker1'}]}
+    expected_output = { "dms" : [{'dm_id': 1, 'name': 'fakerisafaker, fakerisafaker1'}]}
     
     assert list_of_dicts == expected_output
 
-    def test_succesful_case2():
-        requests.delete(f"{url}/clear/v1")
+def test_succesful_case2():
+    requests.delete(f"{url}/clear/v1")
 
-        new_user = {"email" : "fakeguy@gmail.com" , "password": "fake12345","name_first" : "faker", "name_last" : "is_a_faker" }
-        new_user1 = {"email" : "fakeguy1@gmail.com" , "password": "fake123456","name_first" : "faker", "name_last" : "is_a_faker1" }
-        new_user2 = {"email" : "fakeguy2@gmail.com" , "password": "fake123457","name_first" : "faker", "name_last" : "is_a_faker2" }
+    new_user = {"email" : "fakeguy@gmail.com" , "password": "fake12345","name_first" : "faker", "name_last" : "is_a_faker" }
+    new_user1 = {"email" : "fakeguy1@gmail.com" , "password": "fake123456","name_first" : "faker", "name_last" : "is_a_faker1" }
+    new_user2 = {"email" : "fakeguy2@gmail.com" , "password": "fake123457","name_first" : "faker", "name_last" : "is_a_faker2" }
 
-        response = requests.post(f"{url}/auth/register/v2", json=new_user)
-        u_id = response["auth_user_id"]
-        owner_token = response["token"]
-        response = requests.post(f"{url}/auth/register/v2", json=new_user1)
-        u_id1 = response["auth_user_id"]
-        input_token = response["token"]
-        response = requests.post(f"{url}/auth/register/v2", json=new_user2)
-        u_id2 = response["auth_user_id"]
+    response = requests.post(f"{url}/auth/register/v2", json=new_user)
+    u_id = response.json()["auth_user_id"]
+    owner_token = response.json()["token"]
+    response = requests.post(f"{url}/auth/register/v2", json=new_user1)
+    u_id1 = response.json()["auth_user_id"]
+    input_token = response.json()["token"]
+    response = requests.post(f"{url}/auth/register/v2", json=new_user2)
+    u_id2 = response.json()["auth_user_id"]
 
-        response = requests.post(f"{BASE_URL}/dm/create/v1", json={ "token" : owner_token, "u_ids" : [u_id1,u_id2] })
-        response_data = response.json()
-        dm_id = response_data["dm_id"]
+    response = requests.post(f"{BASE_URL}/dm/create/v1", json={ "token" : owner_token, "u_ids" : [u_id1,u_id2] })
+    response_data = response.json()
+        
+    list_of_dicts = requests.get(f"{url}/dm/list/v1", params={"token" : input_token })
 
-        list_of_dicts = requests.get(f"{url}/dm/list/v1", params={"token" : input_token })
+    output = list_of_dicts.json()
 
-        output = list_of_dicts.json()
+    expected_output = {"dms" : [{"dm_id" : 1 , 'name': "fakerisafaker, fakerisafaker1, fakerisafaker2" }]}
 
-        expected_output = {"dms" : [{"dm_id" : 1 , 'name': "faker1isafaker , faker1isafaker1 ,fakerisafaker2" }]}
+    assert expected_output == output
 
-        list_of_dicts = requests.get(f"{url}/dm/list/v1", params={"token" : input_token })
+#pass token through auth user logut to invalidate sessions
 
-        assert expected_output == list_of_dicts
+def test_successfull_case3():
+    requests.delete(f"{url}/clear/v1")
+
+    new_user = {"email" : "fakeguy@gmail.com" , "password": "fake12345","name_first" : "faker", "name_last" : "is_a_faker" }
+    new_user1 = {"email" : "fakeguy1@gmail.com" , "password": "fake123456","name_first" : "faker", "name_last" : "is_a_faker1" }
+    new_user2 = {"email" : "fakeguy2@gmail.com" , "password": "fake123457","name_first" : "faker", "name_last" : "is_a_faker2" }
+
+    response = requests.post(f"{url}/auth/register/v2", json=new_user)
+    u_id = response.json()["auth_user_id"]
+    owner_token = response.json()["token"]
+    response = requests.post(f"{url}/auth/register/v2", json=new_user1)
+    u_id1 = response.json()["auth_user_id"]
+    input_token = response.json()["token"]
+    response = requests.post(f"{url}/auth/register/v2", json=new_user2)
+    u_id2 = response.json()["auth_user_id"]  
+
+    response = requests.post(f"{BASE_URL}/dm/create/v1", json={ "token" : owner_token, "u_ids" : [u_id1,u_id2] })
+    response_data = response.json()
+    response = requests.post(f"{BASE_URL}/dm/create/v1", json={ "token" : owner_token, "u_ids" : [u_id2] })
+    response_data = response.json()
+    
+    list_of_dicts = requests.get(f"{url}/dm/list/v1", params={"token" : input_token })
+
+    output = list_of_dicts.json()
+
+    expected_output = {"dms" : [{"dm_id" : 1 , 'name': "fakerisafaker, fakerisafaker1, fakerisafaker2" }]}
+
+    assert expected_output == output
+
+def test_just_owner():
+    requests.delete(f"{url}/clear/v1")
+
+    new_user = {"email" : "fakeguy@gmail.com" , "password": "fake12345","name_first" : "faker", "name_last" : "is_a_faker" }
+
+    response = requests.post(f"{url}/auth/register/v2", json=new_user)
+    u_id = response.json()["auth_user_id"]
+    owner_token = response.json()["token"] 
+
+    response = requests.post(f"{BASE_URL}/dm/create/v1", json={ "token" : owner_token, "u_ids" : [] })
+    response_data = response.json()
+    
+    list_of_dicts = requests.get(f"{url}/dm/list/v1", params={"token" : owner_token })
+
+    output = list_of_dicts.json()
+
+    expected_output = {"dms" : [{"dm_id" : 1 , 'name': "fakerisafaker" }]}
+
+    assert expected_output == output
+
+
+def test_logged_out_creator():
+    requests.delete(f"{url}/clear/v1")
+
+    new_user = {"email" : "fakeguy@gmail.com" , "password": "fake12345","name_first" : "faker", "name_last" : "is_a_faker" }
+
+    response = requests.post(f"{url}/auth/register/v2", json=new_user)
+
+    response_data = response.json()
+    new_user_token = {"token" : response_data["token"]}
+
+    response = requests.post(f"{BASE_URL}/dm/create/v1", json={ "token" : new_user_token, "u_ids" : [] })
+    
+    response = requests.post(f"{url}/auth/logout/v1", json=new_user_token)
+
+    response = requests.get(f"{url}/dm/list/v1", params={"token" : new_user_token })
+
+    assert response.status_code == 403 
+
+
+
+
+
+
 
 
 
