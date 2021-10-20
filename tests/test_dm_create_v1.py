f56daeec1f566c68244bc81d1f4838e112e16a00
Abbas Zaidi <abbas.zaidi1@student.unsw.edu.au>
2021-10-20 13:09:58 +1100

    added more test casses using the functions return aswell as dm list

--- tests/test_dm_create_v1.py
+++ tests/test_dm_create_v1.py
@@ -1,5 +1,6 @@
 import requests
 import pytest
+from requests.sessions import extract_cookies_to_jar
 from src.config import url
 
 BASE_URL = url
@@ -33,12 +34,61 @@ def test_one_created_dm():
     response_data = response.json()
     new_user_token = response_data["token"]
 
-    response = requests.post(f"{url}/dm/create/v1", json=new_user_token) 
+    response = requests.post(f"{url}/dm/create/v1", json={"token" : new_user_token, "u_ids" : []}) 
     dm_id = response.json()
 
-    print(dm_id)
+    assert dm_id["dm_id"] == 1
 
-    assert dm_id == 1
+def test_two_created_dm():
+    requests.delete(f"{url}/clear/v1")
 
+    user1 = {"email" : "fakeguy@gmail.com" , "password": "fake12345","name_first" : "faker", "name_last" : "is_a_faker" }
+    user2 = {"email" : "fakeguy1@gmail.com" , "password": "fake123456","name_first" : "faker", "name_last" : "is_a_faker1" }
 
+    response = requests.post(f"{url}/auth/register/v2", json=user1) 
+    response_data = response.json()
+    user1_token = response_data["token"]
 
+    response = requests.post(f"{url}/auth/register/v2", json=user2) 
+    response_data = response.json()
+    user2_id = response_data["auth_user_id"]
+
+    response = requests.post(f"{url}/dm/create/v1", json={"token" : user1_token, "u_ids" : []}) 
+    
+    response = requests.post(f"{url}/dm/create/v1", json={"token" : user1_token, "u_ids" : [user2_id]}) 
+    dm_id = response.json()
+
+    assert dm_id["dm_id"] == 2
+
+def test_two_identical_dms():
+    requests.delete(f"{url}/clear/v1")
+
+    new_user = {"email" : "fakeguy@gmail.com" , "password": "fake12345","name_first" : "faker", "name_last" : "is_a_faker" }
+
+    response = requests.post(f"{url}/auth/register/v2", json=new_user) 
+    response_data = response.json()
+    new_user_token = response_data["token"]
+
+    response = requests.post(f"{url}/dm/create/v1", json={"token" : new_user_token, "u_ids" : []}) 
+    response = requests.post(f"{url}/dm/create/v1", json={"token" : new_user_token, "u_ids" : []}) 
+
+    dm_id = response.json()
+
+    assert dm_id["dm_id"] == 2
+
+def test_two_identical_dms():
+    requests.delete(f"{url}/clear/v1")
+
+    new_user = {"email" : "fakeguy@gmail.com" , "password": "fake12345","name_first" : "faker", "name_last" : "is_a_faker" }
+
+    response = requests.post(f"{url}/auth/register/v2", json=new_user) 
+    response_data = response.json()
+    new_user_token = response_data["token"]
+
+    response = requests.post(f"{url}/dm/create/v1", json={"token" : new_user_token, "u_ids" : []}) 
+    response = requests.post(f"{url}/dm/create/v1", json={"token" : new_user_token, "u_ids" : []}) 
+
+    list_of_dicts = requests.get(f"{url}/dm/list/v1", params={"token" : new_user_token })
+    expected_output = {"dms" : [{"dm_id" : 1, "name" : "fakerisafaker"},{"dm_id" : 2, "name" : "fakerisafaker"}]}
+
+    assert expected_output == list_of_dicts.json()
