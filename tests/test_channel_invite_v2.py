04b1ed93ae9bc821eba557b7529437f4d9250672
Abbas Zaidi <abbas.zaidi1@student.unsw.edu.au>
2021-10-23 19:33:28 +1100

    added pytest and helper function and helper function within the pytest

--- tests/test_channel_invite_v2.py
+++ tests/test_channel_invite_v2.py
@@ -12,56 +12,37 @@ def clear():
     requests.delete(f"{BASE_URL}/clear/v1")
 @pytest.fixture
 def user1():
-    new_user = {"email" : "fakeguy@gmail.com" , "password": "fake12345","name_first" : "faker", "name_last" : "is_a_faker" }
-    response = requests.post(f"{BASE_URL}/auth/register/v2", json=new_user)
+    response = rh.auth_register("fakeguy@gmail.com","fake12345","faker","is_a_faker")
     return response.json()
 @pytest.fixture
 def user2():
-    new_user1 = {"email" : "fakeguy1@gmail.com" , "password": "fake123451","name_first" : "faker", "name_last" : "is_a_faker1" }   
-    response = requests.post(f"{BASE_URL}/auth/register/v2", json=new_user1)
+    response = rh.auth_register("fakeguy1@gmail.com","fake123451","faker","is_a_faker1")
+    return response.json()
+@pytest.fixture
+def user3():   
+    response = rh.auth_register("fakeguy2@gmail.com","fake123452","faker","is_a_faker2")
     return response.json()
 #==test==#
-def test_invalid_channel(clear,user1):
-    #requests.delete(f"{url}/clear/v1")
-
-    #new_user = {"email" : "fakeguy@gmail.com" , "password": "fake12345","name_first" : "faker", "name_last" : "is_a_faker" }
-
-    #response = requests.post(f"{url}/auth/register/v2", json=new_user) 
-    #response_data = response.json()
+def test_invalid_channel(clear,user1,user2):
     auth_user_token = user1["token"]
 
-    #channel_data = {"token" : auth_user_token, "name" : "joshuasucks", "is_public": True} 
-
-    #response = requests.post(f"{url}/channels/create/v2", json=channel_data)
-    response = rh.channels_create()
+    response = rh.channels_create(auth_user_token,"fakeguy",True)
     response_data = response.json()
 
     invalid_channel_id = response_data["channel_id"] + 1
 
-    new_user1 = {"email" : "fakeguy1@gmail.com" , "password": "fake123456","name_first" : "faker1", "name_last" : "is_a_faker1" }
-    response = requests.post(f"{url}/auth/register/v2", json=new_user1) 
-    response_data = response.json()
-    new_user_id = response_data["auth_user_id"]
+    new_user_id = user2["auth_user_id"]
 
     response = requests.post(f"{url}/channel/invite/v2", json={"token": auth_user_token, "channel_id": invalid_channel_id, "u_id" : new_user_id})
     
 
     assert response.status_code == 400
 
-def test_already_member():
-    requests.delete(f"{url}/clear/v1")
+def test_already_member(clear, user1):
+    auth_user_token = user1["token"]
+    new_user_id = user1["auth_user_id"]
 
-    new_user = {"email" : "fakeguy@gmail.com" , "password": "fake12345","name_first" : "faker", "name_last" : "is_a_faker" }
-
-    response = requests.post(f"{url}/auth/register/v2", json=new_user) 
-    response_data = response.json()
-    auth_user_token = response_data["token"]
-    new_user_id = response_data["auth_user_id"]
-
-    channel_data = {"token" : auth_user_token, "name" : "joshuasucks", "is_public": True} 
-
-    response = requests.post(f"{url}/channels/create/v2", json=channel_data)
-
+    response = rh.channels_create(auth_user_token,"fakeguy",True)
     response_data = response.json()
 
     channel_id = response_data["channel_id"]
@@ -70,19 +51,11 @@ def test_already_member():
 
     assert response.status_code == 400
 
-def test_invalid_user_id():
-    requests.delete(f"{url}/clear/v1")
+def test_invalid_user_id(clear, user1):
+    auth_user_token = user1["token"]
+    new_user_id = user1["auth_user_id"]
 
-    new_user = {"email" : "fakeguy@gmail.com" , "password": "fake12345","name_first" : "faker", "name_last" : "is_a_faker" }
-
-    response = requests.post(f"{url}/auth/register/v2", json=new_user) 
-    response_data = response.json()
-    auth_user_token = response_data["token"]
-    new_user_id = response_data["auth_user_id"]
-    channel_data = {"token" : auth_user_token, "name" : "joshuasucks", "is_public": True} 
-
-    response = requests.post(f"{url}/channels/create/v2", json=channel_data)
-
+    response = rh.channels_create(auth_user_token,"fakeguy",True)
     response_data = response.json()
 
     channel_id = response_data["channel_id"]
@@ -93,56 +66,31 @@ def test_invalid_user_id():
 
     assert response.status_code == 400
 
-def test_not_auth_user():
-
-    requests.delete(f"{url}/clear/v1")
-
-    new_user = {"email" : "fakeguy@gmail.com" , "password": "fake12345","name_first" : "faker", "name_last" : "is_a_faker" }
-
-    response = requests.post(f"{url}/auth/register/v2", json=new_user) 
-    response_data = response.json()
-    auth_user_token = response_data["token"]
-    channel_data = {"token" : auth_user_token, "name" : "joshuasucks", "is_public": True} 
-
-    response = requests.post(f"{url}/channels/create/v2", json=channel_data)
+def test_not_auth_user(clear,user1,user2,user3):
+    auth_user_token = user1["token"]
+    
+    response = rh.channels_create(auth_user_token,"fakeguy",True)
 
     response_data = response.json()
 
     channel_id = response_data["channel_id"]
 
-    new_user = {"email" : "fakeguy1@gmail.com" , "password": "fake12345","name_first" : "faker1", "name_last" : "is_a_faker" }
+    invalid_auth_id = user2["auth_user_id"] 
 
-    invalid_auth_id = requests.post(f"{url}/auth/register/v2", json=new_user).json()["token"] 
+    valid_uid = user3["auth_user_id"]
 
-    new_user = {"email" : "fakeguy2@gmail.com" , "password": "fake12345","name_first" : "faker2", "name_last" : "is_a_faker" }
-
-    valid_uid = requests.post(f"{url}/auth/register/v2", json=new_user).json()["auth_user_id"]
-
-    response = requests.post(f"{url}/channel/invite/v2", json={"token": invalid_auth_id, "channel_id": channel_id, "u_id" : valid_uid})
-
+    response = rh.channel_invite(invalid_auth_id,channel_id,valid_uid)
     assert response.status_code == 403
 
-def test_positive_casses():
-    requests.delete(f"{url}/clear/v1")
+def test_positive_casses(clear,user1,user2):
+    auth_user_token = user1["token"]
 
-    new_user = {"email" : "fakeguy@gmail.com" , "password": "fake12345","name_first" : "faker", "name_last" : "is_a_faker" }
-
-    response = requests.post(f"{url}/auth/register/v2", json=new_user) 
-    response_data = response.json()
-    auth_user_token = response_data["token"]
-    channel_data = {"token" : auth_user_token, "name" : "joshuasucks", "is_public": True} 
-
-    response = requests.post(f"{url}/channels/create/v2", json=channel_data)
+    response = rh.channels_create(auth_user_token,"fakeguy",True)
 
     response_data = response.json()
     channel_id = response_data["channel_id"]
-    new_user1 = {"email" : "fakeguy1@gmail.com" , "password": "fake123456","name_first" : "faker1", "name_last" : "is_a_faker1" }
 
-    response = requests.post(f"{url}/auth/register/v2", json=new_user1) 
-
-    response_data = response.json()
-
-    new_user_id = response_data["auth_user_id"]
+    new_user_id = user2["auth_user_id"]
 
     response = requests.post(f"{url}/channel/invite/v2", json={"token": auth_user_token, "channel_id":channel_id , "u_id": new_user_id })
 
