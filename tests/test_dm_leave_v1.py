9366f6b9c783b3e95182d795ceec941a7234a520
Keefe Vuong <z5360545@ad.unsw.edu.au>
2021-10-24 01:24:01 +1100

    Added pytest fixtures

--- tests/test_dm_leave_v1.py
+++ tests/test_dm_leave_v1.py
@@ -1,39 +1,46 @@
 import pytest
-import requests
 from src.config import url
 import tests.route_helpers as rh
 
 BASE_URL = url
 
-def test_dm_leave_multiple_dms_valid():
-    rh.clear()
+@pytest.fixture
+def clear():
+    return rh.clear()
 
+@pytest.fixture
+def first_user_data():
     response = rh.auth_register("keefe@gmail.com", "password", "keefe", "vuong")
-    response_data = response.json()
-    owner_token = response_data["token"]  
-    owner_u_id = response_data["auth_user_id"]
+    return response.json()
 
+@pytest.fixture
+def second_user_data():
     response = rh.auth_register("eagle@gmail.com", "password", "team", "eagle")
-    response_data = response.json()
-    invited_member_token = response_data["token"] 
-    invited_member_u_id = response_data["auth_user_id"]
+    return response.json()  
 
-    response = rh.dm_create(owner_token, [invited_member_u_id])
+@pytest.fixture
+def third_user_data():
+    response = rh.auth_register("butcher@gmail.com", "password", "knife", "butcher")
+    return response.json()
+
+def test_dm_leave_multiple_dms_valid(clear, first_user_data, second_user_data):
+
+    response = rh.dm_create(first_user_data["token"], [second_user_data["auth_user_id"]])
     response_data = response.json()
     dm_id = response_data["dm_id"]
 
-    response = rh.dm_create(invited_member_token, [owner_u_id])
+    response = rh.dm_create(second_user_data["token"], [first_user_data["auth_user_id"]])
 
-    response = rh.dm_leave(invited_member_token, dm_id)
+    response = rh.dm_leave(second_user_data["token"], dm_id)
 
-    response = rh.dm_details(owner_token, dm_id)
+    response = rh.dm_details(first_user_data["token"], dm_id)
     response_data = response.json()
 
     expected_output = {
         "name": "keefevuong, teameagle",
         "members": [
             {
-                'u_id': owner_u_id,
+                'u_id': first_user_data["auth_user_id"],
                 'email': 'keefe@gmail.com',
                 'name_first': 'keefe',
                 'name_last': 'vuong',
@@ -44,62 +51,29 @@ def test_dm_leave_multiple_dms_valid():
 
     assert response_data == expected_output
 
-def test_dm_leave_invalid_dm_id():
-    rh.clear()
+def test_dm_leave_invalid_dm_id(clear, first_user_data, second_user_data):
 
-    response = rh.auth_register("keefe@gmail.com", "password", "keefe", "vuong")
-    response_data = response.json()
-    owner_token = response_data["token"]  
-
-    response = rh.auth_register("eagle@gmail.com", "password", "team", "eagle")
-    response_data = response.json()
-    invited_member_token = response_data["token"]  
-    invited_member_u_id = response_data["auth_user_id"]
-
-    response = rh.dm_create(owner_token, [invited_member_u_id])
-    response_data = response.json()
+    response = rh.dm_create(first_user_data["token"], [second_user_data["auth_user_id"]])
 
     fake_dm_id = 9999
 
-    response = rh.dm_leave(invited_member_token, fake_dm_id)
+    response = rh.dm_leave(second_user_data["token"], fake_dm_id)
 
     assert response.status_code == 400
 
-def test_dm_leave_not_a_member_of_dm():
-    rh.clear()
+def test_dm_leave_not_a_member_of_dm(clear, first_user_data, second_user_data, third_user_data):
 
-    response = rh.auth_register("keefe@gmail.com", "password", "keefe", "vuong")
-    response_data = response.json()
-    owner_token = response_data["token"]  
-
-    response = rh.auth_register("eagle@gmail.com", "password", "team", "eagle")
-    response_data = response.json()
-    invited_member_u_id = response_data["auth_user_id"]
-
-    response = rh.auth_register("butcher@gmail.com", "password", "knife", "butcher")
-    response_data = response.json()
-    uninvited_member_token = response_data["token"]
-
-    response = rh.dm_create(owner_token, [invited_member_u_id])
+    response = rh.dm_create(first_user_data["token"], [second_user_data["auth_user_id"]])
     response_data = response.json()
     dm_id = response_data["dm_id"]
 
-    response = rh.dm_leave(uninvited_member_token, dm_id)
+    response = rh.dm_leave(third_user_data["token"], dm_id)
 
     assert response.status_code == 403
 
-def test_dm_leave_unauthorised_token():
-    rh.clear()
+def test_dm_leave_unauthorised_token(clear, first_user_data, second_user_data):
 
-    response = rh.auth_register("keefe@gmail.com", "password", "keefe", "vuong")
-    response_data = response.json()
-    owner_token = response_data["token"]
-
-    response = rh.auth_register("eagle@gmail.com", "password", "team", "eagle")
-    response_data = response.json()
-    invited_member_u_id = response_data["auth_user_id"]
-
-    response = rh.dm_create(owner_token, [invited_member_u_id])
+    response = rh.dm_create(first_user_data["token"], [second_user_data["auth_user_id"]])
     response_data = response.json()
     dm_id = response_data["dm_id"]
     
@@ -109,33 +83,22 @@ def test_dm_leave_unauthorised_token():
 
     assert response.status_code == 403
 
-def test_dm_leave_valid():
-    rh.clear()
+def test_dm_leave_valid(clear, first_user_data, second_user_data):
 
-    response = rh.auth_register("keefe@gmail.com", "password", "keefe", "vuong")
-    response_data = response.json()
-    owner_token = response_data["token"]
-    owner_u_id = response_data["auth_user_id"]
-
-    response = rh.auth_register("eagle@gmail.com", "password", "team", "eagle")
-    response_data = response.json()
-    invited_member_token = response_data["token"]  
-    invited_member_u_id = response_data["auth_user_id"]
-
-    response = rh.dm_create(owner_token, [invited_member_u_id])
+    response = rh.dm_create(first_user_data["token"], [second_user_data["auth_user_id"]])
     response_data = response.json()
     dm_id = response_data["dm_id"]
 
-    response = rh.dm_leave(invited_member_token, dm_id)
+    response = rh.dm_leave(second_user_data["token"], dm_id)
 
-    response = rh.dm_details(owner_token, dm_id)
+    response = rh.dm_details(first_user_data["token"], dm_id)
     response_data = response.json()
 
     expected_output = {
         "name": "keefevuong, teameagle",
         "members": [
             {
-                'u_id': owner_u_id,
+                'u_id': first_user_data["auth_user_id"],
                 'email': 'keefe@gmail.com',
                 'name_first': 'keefe',
                 'name_last': 'vuong',
@@ -146,32 +109,22 @@ def test_dm_leave_valid():
 
     assert response_data == expected_output
 
-def test_dm_leave_owner_leaves_dm():
-    rh.clear()
+def test_dm_leave_owner_leaves_dm(clear, first_user_data, second_user_data):
 
-    response = rh.auth_register("keefe@gmail.com", "password", "keefe", "vuong")
-    response_data = response.json()
-    owner_token = response_data["token"]  
-
-    response = rh.auth_register("eagle@gmail.com", "password", "team", "eagle")
-    response_data = response.json()
-    invited_member_token = response_data["token"]  
-    invited_member_u_id = response_data["auth_user_id"]
-
-    response = rh.dm_create(owner_token, [invited_member_u_id])
+    response = rh.dm_create(first_user_data["token"], [second_user_data["auth_user_id"]])
     response_data = response.json()
     dm_id = response_data["dm_id"]
 
-    response = rh.dm_leave(owner_token, dm_id) 
+    response = rh.dm_leave(first_user_data["token"], dm_id) 
 
-    response = rh.dm_details(invited_member_token, dm_id)
+    response = rh.dm_details(second_user_data["token"], dm_id)
     response_data = response.json()
 
     expected_output = {
         "name": "keefevuong, teameagle",
         "members": [
             {
-                'u_id': invited_member_u_id,
+                'u_id': second_user_data["auth_user_id"],
                 'email': 'eagle@gmail.com',
                 'name_first': 'team',
                 'name_last': 'eagle',
