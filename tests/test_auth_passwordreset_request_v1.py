4922bcf6a546a1ea059ffef1ecdc023a5c3984f8
z5362176 <z5362176@cse.unsw.edu.au>
2021-11-02 17:00:01 +1100

    Added another test case for logging out user's sessions if provided emails corresponds to that user

--- tests/test_auth_passwordreset_request_v1.py
+++ tests/test_auth_passwordreset_request_v1.py
@@ -7,3 +7,14 @@ def test_invalid_and_valid_email():
     assert rh.auth_passwordreset_request("random@gmail.com").status_code == 200
     assert rh.auth_passwordreset_request("bademail@gmail.com").status_code == 200
     assert rh.auth_passwordreset_request("bademail").status_code == 200
+
+# If a valid email is given, it will log out any ongoing sessions corresponding to that
+# email.
+def test_passwordreset_request_logs_out_all_sessions():
+    rh.clear()
+    token = rh.auth_register("random@gmail.com", "123abc!@#", "John", "Smith").json()["token"]
+
+    rh.auth_passwordreset_request("random@gmail.com")
+
+    assert rh.channels_create(token, "channel", True).status_code == 403
+    
