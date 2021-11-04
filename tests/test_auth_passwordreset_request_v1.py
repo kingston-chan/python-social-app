869d6fe0d0748d67a20ea4d99195b4f74bf25ae7
z5362176 <z5362176@cse.unsw.edu.au>
2021-11-04 19:11:04 +1100

    Added pytest fixture

--- tests/test_auth_passwordreset_request_v1.py
+++ tests/test_auth_passwordreset_request_v1.py
@@ -1,25 +1,24 @@
 import tests.route_helpers as rh
+import pytest
 
-def test_invalid_and_valid_email():
+@pytest.fixture
+def clear_and_register():
     rh.clear()
-    rh.auth_register("random@gmail.com", "123abc!@#", "John", "Smith")
+    return rh.auth_register("random@gmail.com", "123abc!@#", "John", "Smith").json()["token"]
 
+def test_invalid_and_valid_email(clear_and_register):
     assert rh.auth_passwordreset_request("random@gmail.com").status_code == 200
     assert rh.auth_passwordreset_request("bademail@gmail.com").status_code == 200
     assert rh.auth_passwordreset_request("bademail").status_code == 200
 
 # If a valid email is given, it will log out any ongoing sessions corresponding to that
 # email.
-def test_passwordreset_request_logs_out_all_sessions():
-    rh.clear()
-    token = rh.auth_register("random@gmail.com", "123abc!@#", "John", "Smith").json()["token"]
-
+def test_passwordreset_request_logs_out_all_sessions(clear_and_register):
     rh.auth_passwordreset_request("random@gmail.com")
+    assert rh.channels_create(clear_and_register, "channel", True).status_code == 403
 
-    assert rh.channels_create(token, "channel", True).status_code == 403
-
+# Working email
 def test_works():
     rh.clear()
     rh.auth_register("h13beaglestreamsuser@gmail.com", "123abc!@#", "John", "Smith").json()["token"]
-
     assert rh.auth_passwordreset_request("h13beaglestreamsuser@gmail.com").status_code == 200
