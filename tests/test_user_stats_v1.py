8a1bec53fa58018531e366c81248ba4db1d16c86
Julian Winzer <z5371533@ad.unsw.edu.au>
2021-11-08 23:14:47 +1100

    Fixed tests to better match the spec

--- tests/test_user_stats_v1.py
+++ tests/test_user_stats_v1.py
@@ -1,5 +1,8 @@
 import tests.route_helpers as rh
 import pytest, time
+@pytest.fixture
+def clear():
+    return rh.clear()
 
 @pytest.fixture
 def clear_and_register():
@@ -11,6 +14,17 @@ def create_channel(clear_and_register):
     return rh.channels_create(clear_and_register, "channel", True).json()["channel_id"]
 
 @pytest.fixture
+def first_user_data():
+    response = rh.auth_register("email1@email.com", "password", "userOne", "lastOne")
+    return response.json()  
+
+@pytest.fixture
+def second_user_data():
+    response = rh.auth_register("email2@email.com", "password", "userTwo", "lastTwo")
+    return response.json()  
+
+
+@pytest.fixture
 def create_dm(clear_and_register):
     return rh.dm_create(clear_and_register, []).json()["dm_id"]
 
@@ -18,16 +32,15 @@ def create_dm(clear_and_register):
 
 # Metrics when first user registers and does not join dm or channel
 def test_user_stats_register_user(clear_and_register):
-    user_1 = rh.users_stats(clear_and_register).json()["user_stats"]
+    user_1 = rh.user_stats(clear_and_register).json()["user_stats"]
     assert len(user_1["channels_joined"]) == 1
     assert user_1["channels_joined"][-1]["num_channels_joined"] == 0
     
 
 # Metrics for channels and dms work
 def test_user_stats_join_channel_dm(clear_and_register, create_channel, create_dm):
-    user = rh.users_stats(clear_and_register).json()["user_stats"]
-    # two time stamps because 1 when first user registers and second comes from create 
-    # new channel/dm
+    user = rh.user_stats(clear_and_register).json()["user_stats"]
+  
     assert len(user["channels_joined"]) == 2
     assert user["channels_joined"][-1]["num_channels_joined"] == 1
 
@@ -39,17 +52,19 @@ def test_user_stats_join_channel_dm(clear_and_register, create_channel, create_d
     assert user["involvement_rate"] == 1
 
 # Metrics for messages works
-def test_user_stats_messages_metrics(clear_and_register, create_channel):
-    rh.message_send(clear_and_register, create_channel, "hello")
+def test_user_stats_messages_metrics(clear, first_user_data):
+    response = rh.channels_create(first_user_data["token"], "channel_1", True)
+    channel_1 = response.json()["channel_id"]
+    rh.message_send(first_user_data["token"], channel_1, "hello")
     time_message_1 = time.time()
-    rh.message_send(clear_and_register, create_channel, "hello")
+    rh.message_send(first_user_data["token"], channel_1, "hello")
     time_message_2 = time.time()
-    rh.message_send(clear_and_register, create_channel, "hello")
+    rh.message_send(first_user_data["token"], channel_1, "hello")
     time_message_3 = time.time()
-    rh.message_send(clear_and_register, create_channel, "hello")
+    rh.message_send(first_user_data["token"], channel_1, "hello")
     time_message_4 = time.time()
 
-    user = rh.users_stats(clear_and_register).json()["user_stats"]
+    user = rh.user_stats(first_user_data["token"]).json()["user_stats"]
     # seperate time stamp for each message sent
     assert len(user["messages_sent"]) == 5
 
@@ -67,10 +82,26 @@ def test_user_stats_messages_metrics(clear_and_register, create_channel):
     assert user["involvement_rate"] == 1
 
 # involvement rate changes correctly
-def test_user_stats_involvement_rate_change(clear_and_register, create_dm):
-    assert rh.users_stats(clear_and_register).json()["user_stats"]["involvement_rate"] == 1
-    rh.auth_register("random2@gmail.com", "123abc!@#", "Bob", "Smith")
-    assert rh.users_stats(clear_and_register).json()["user_stats"]["involvement_rate"] == 0.5
+def test_user_stats_involvement_rate_change(clear, first_user_data, second_user_data):
+    assert rh.user_stats(first_user_data["token"]).json()["user_stats"]["involvement_rate"] == 0
+    rh.channels_create(second_user_data["token"], "channel_1", True)
+    assert rh.user_stats(second_user_data["token"]).json()["user_stats"]["involvement_rate"] == 1
+    rh.channels_create(first_user_data["token"], "channel_2", True)
+    assert rh.user_stats(first_user_data["token"]).json()["user_stats"]["involvement_rate"] == 0.5
+
+def test_user_stats_messages_from_multiple_users(clear, first_user_data, second_user_data):
+    response = rh.channels_create(first_user_data["token"], "channel_1", True)
+    channel_1 = response.json()["channel_id"]
+    rh.message_send(first_user_data["token"], channel_1, "hello")
+
+    user = rh.user_stats(first_user_data["token"]).json()["user_stats"]
+    assert user["messages_sent"][-1]["num_messages_sent"] == 1
+
+    rh.channel_join(second_user_data["token"], channel_1)
+    rh.message_send(second_user_data["token"], channel_1, "hello")
+
+    user = rh.user_stats(first_user_data["token"]).json()["user_stats"]
+    assert user["messages_sent"][-1]["num_messages_sent"] == 1
 
 # Number of messages and dms decrease if they are removed
 def test_user_stats_num_dms_messages_decrease(clear_and_register, create_dm, create_channel):
@@ -81,7 +112,7 @@ def test_user_stats_num_dms_messages_decrease(clear_and_register, create_dm, cre
     og_msg = rh.message_send(clear_and_register, create_channel, "hello").json()["message_id"]
     removing_message_id = rh.message_send(clear_and_register, create_channel, "hello").json()["message_id"]
 
-    user = rh.users_stats(clear_and_register).json()["user_stats"]
+    user = rh.user_stats(clear_and_register).json()["user_stats"]
     assert user["dms_joined"][-1]["num_dms_joined"] == 1
     assert user["messages_sent"][-1]["num_messages_sent"] == 5
 
@@ -89,40 +120,33 @@ def test_user_stats_num_dms_messages_decrease(clear_and_register, create_dm, cre
     rh.message_remove(clear_and_register, removing_message_id)
     rh.message_edit(clear_and_register, og_msg, "bye")
     rh.message_share(clear_and_register, og_msg, "hello", create_channel, -1)
-
-    user = rh.users_stats(clear_and_register).json()["user_stats"]
+    print(rh.user_stats(clear_and_register).json()["user_stats"])
+    user = rh.user_stats(clear_and_register).json()["user_stats"]
     assert user["dms_joined"][-1]["num_dms_joined"] == 0
-    assert user["messages_sent"][-1]["num_messages_sent"] == 2
+    assert user["messages_sent"][-1]["num_messages_sent"] == 6
 
-# involvement increases if removed user is not in a dm/channel
-def test_user_stats_involvement_stays_same(clear_and_register, create_channel):
-    uid2 = rh.auth_register("random2@gmail.com", "123abc!@#", "Bob", "Smith").json()["auth_user_id"]
-    assert rh.users_stats(clear_and_register).json()["user_stats"]["involvement_rate"] == 0.5
-    rh.admin_user_remove(clear_and_register, uid2)
-    assert rh.users_stats(clear_and_register).json()["user_stats"]["involvement_rate"] == 1
 
 
 # user changes only when messages are sent
 def test_user_stats_with_delayed_messages(clear_and_register, create_channel, create_dm):
     # Send a message in the dm and channel in three seconds
-    rh.message_sendlater(clear_and_register, create_channel, "hello", int(time.time()) + 3)
-    rh.message_sendlaterdm(clear_and_register, create_dm, "hello", int(time.time()) + 4)
-
-    assert len(rh.users_stats(clear_and_register).json()["user_stats"]["messages_sent"]) == 1
-    assert rh.users_stats(clear_and_register).json()["user_stats"]["messages_sent"][-1]["num_messages_sent"] == 0
-
+    rh.message_sendlater(clear_and_register, create_channel, "hello", int(time.time()) + 2)
+    rh.message_sendlaterdm(clear_and_register, create_dm, "hello", int(time.time()) + 3)
+    assert len(rh.user_stats(clear_and_register).json()["user_stats"]["messages_sent"]) == 1
+    assert rh.user_stats(clear_and_register).json()["user_stats"]["messages_sent"][-1]["num_messages_sent"] == 0
     time.sleep(4)
-    assert len(rh.users_stats(clear_and_register).json()["user_stats"]["messages_sent"]) == 3
-    assert rh.users_stats(clear_and_register).json()["user_stats"]["messages_sent"][-1]["num_messages_sent"] == 2
+    print(rh.user_stats(clear_and_register).json()["user_stats"])
+    assert len(rh.user_stats(clear_and_register).json()["user_stats"]["messages_sent"]) == 3
+    assert rh.user_stats(clear_and_register).json()["user_stats"]["messages_sent"][-1]["num_messages_sent"] == 2
 
 # Invalid inputs
 
 # Invalid token
 def test_user_stats_invalid_token(clear_and_register):
-    assert rh.users_stats(clear_and_register + 'invalidtoken').status_code == 403
+    assert rh.user_stats(clear_and_register + 'invalidtoken').status_code == 403
 
 # Invalid session
 def test_user_stats_invalid_session(clear_and_register):
     rh.auth_logout(clear_and_register)
-    assert rh.users_stats(clear_and_register).status_code == 403
+    assert rh.user_stats(clear_and_register).status_code == 403
 
