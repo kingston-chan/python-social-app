ab57b3f15f499dfb3c9bc760e135aa2f7a7c4099
Joshua Cruzado <z5367218@ad.unsw.edu.au>
2021-11-14 18:23:13 +1100

    Added and modified tests for channel_unban.

--- tests/test_channel_unban_v1.py
+++ tests/test_channel_unban_v1.py
@@ -2,7 +2,7 @@ import pytest
 import tests.route_helpers as rh
 from src.config import url
 BASE_URL = url
-## =====[ test_channel_ban_v1.py ]===== ##
+## =====[ test_channel_unban_v1.py ]===== ##
 
 # ==== Fixtures ==== #
 @pytest.fixture
@@ -22,56 +22,59 @@ def user3():
     return rh.auth_register("user3@email.com", "password", "user", "three").json()
 
 # ==== Helper Functions ==== #
-def create_channel(creator_token, invited_members, is_public):
+def create_channel_and_ban(creator_token, invited_members, is_public):
     channel_id = rh.channels_create(creator_token, "chan_name", is_public).json()['channel_id']
     if is_public:
         for member in invited_members:
             rh.channel_join(member, channel_id)
+            rh.channel_ban(creator_token, channel_id, member)
     else:
         for member in invited_members:
             rh.channel_invite(creator_token, channel_id, member)
+            rh.channel_ban(creator_token, channel_id, member)
     return channel_id
 
 # ==== Tests - Errors ==== #
 ## Input Error - 400 ##
 def test_invalid_channel(clear, user1, user2):
-    channel_id = create_channel(user1["token"], [user2["token"]], True)
-    ban_response = rh.channel_ban(user1["token"], int(channel_id + 1), user2["auth_user_id"])
-    assert ban_response.status_code == 400
+    channel_id = create_channel_and_ban(user1["token"], [user2["token"]], True)
+    unban_response = rh.channel_unban(user1["token"], int(channel_id + 1), user2["auth_user_id"])
+    assert unban_response.status_code == 400
 
-def test_invalid_user_ban(clear, user1, user2):
-    channel_id = create_channel(user1["token"], [], True)
-    ban_response = rh.channel_ban(user1["token"], channel_id, user2["auth_user_id"])
-    assert ban_response.status_code == 400
+def test_invalid_user_unban(clear, user1, user2):
+    channel_id = create_channel_and_ban(user1["token"], [], True)
+    unban_response = rh.channel_unban(user1["token"], channel_id, user2["auth_user_id"])
+    assert unban_response.status_code == 400
 
-def test_self_banning(clear, user1):
-    channel_id = create_channel(user1["token"], [], True)
-    ban_response = rh.channel_ban(user1["token"], channel_id, user1["auth_user_id"])
-    assert ban_response.status_code == 400
+def test_self_unbanning(clear, user1):
+    channel_id = create_channel_and_ban(user1["token"], [], True)
+    unban_response = rh.channel_unban(user1["token"], channel_id, user1["auth_user_id"])
+    assert unban_response.status_code == 400
 
-def test_owner_ban_owner(clear, user1, user2):
+def test_owner_unban_owner(clear, user1, user2):
     channel_id = rh.channels_create(user1["token"], "chan_name", True).json()['channel_id']
     rh.channel_join(user2["token"], channel_id)
     rh.channel_addowner(user1["token"], channel_id, user2["auth_user_id"])
-    ban_response = rh.channel_ban(user2["token"], channel_id, user1["auth_user_id"])
-    assert ban_response.status_code == 400   
-    ban_response = rh.channel_ban(user1["token"], channel_id, user2["auth_user_id"])
-    assert ban_response.status_code == 400
+    unban_response = rh.channel_unban(user2["token"], channel_id, user1["auth_user_id"])
+    assert unban_response.status_code == 400   
+    unban_response = rh.channel_unban(user1["token"], channel_id, user2["auth_user_id"])
+    assert unban_response.status_code == 400
 
 ## Access Error - 403 ##
 def test_unauthorised_user(clear, user1, user2, user3):
-    channel_id = create_channel(user1["token"], [user2["token"]], True)
-    ban_response = rh.channel_ban(user3["token"], channel_id, user2["auth_user_id"])
-    assert ban_response.status_code == 403
+    channel_id = create_channel_and_ban(user1["token"], [user2["token"]], True)
+    unban_response = rh.channel_unban(user3["token"], channel_id, user2["auth_user_id"])
+    assert unban_response.status_code == 403
 
 def test_user_not_owner(clear, user1, user2):
-    channel_id = create_channel(user1["token"], [user2["token"]], True)
-    ban_response = rh.channel_ban(user2["token"], channel_id, user1["auth_user_id"])
-    assert ban_response.status_code == 403
+    channel_id = create_channel_and_ban(user1["token"], [user2["token"]], True)
+    unban_response = rh.channel_unban(user2["token"], channel_id, user1["auth_user_id"])
+    assert unban_response.status_code == 403
 
 # ==== Tests - Valids ==== #
-def test_valid_bans_in_public(clear, user1, user2, user3):
-    channel_id = create_channel(user1["token"], [user2["token"], user3["token"]], True)
+def test_valid_unban_in_public(clear, user1, user2):
+    channel_id = rh.channels_create(user1["token"], "chan_name", True).json()['channel_id']
+    rh.channel_join(user2["token"], channel_id)
 
     expected_output = {
         "name": "chan_name",
@@ -103,17 +106,9 @@ def test_valid_bans_in_public(clear, user1, user2, user3):
                 'handle_str': 'usertwo',
                 'profile_img_url': f"{url}/imgurl/default.jpg"     
             },
-            {
-                'u_id': user3["auth_user_id"],
-                'email': 'user3@email.com',
-                'name_first': 'user',
-                'name_last': 'three',
-                'handle_str': 'userthree',
-                'profile_img_url': f"{url}/imgurl/default.jpg"     
-            },
         ]
     }
-    print(rh.channel_details(user1["token"], channel_id).json())
+
     assert rh.channel_details(user1["token"], channel_id).json() == expected_output
 
     ban_response = rh.channel_ban(user1["token"], channel_id, user2["auth_user_id"])
@@ -139,14 +134,6 @@ def test_valid_bans_in_public(clear, user1, user2, user3):
                 'name_first': 'user',
                 'name_last': 'one',
                 'handle_str': 'userone',
-                'profile_img_url': f"{url}/imgurl/default.jpg"     
-            },
-            {
-                'u_id': user3["auth_user_id"],
-                'email': 'user3@email.com',
-                'name_first': 'user',
-                'name_last': 'three',
-                'handle_str': 'userthree',
                 'profile_img_url': f"{url}/imgurl/default.jpg"     
             },
         ]
@@ -157,8 +144,11 @@ def test_valid_bans_in_public(clear, user1, user2, user3):
     join_response = rh.channel_join(user2["token"], channel_id)
     assert join_response.status_code == 403
 
-    ban_response = rh.channel_ban(user1["token"], channel_id, user3["auth_user_id"])
-    assert ban_response.status_code == 200
+    unban_response = rh.channel_unban(user1["token"], channel_id, user2["auth_user_id"])
+    assert unban_response.status_code == 200
+
+    join_response = rh.channel_join(user2["token"], channel_id)
+    assert join_response.status_code == 200
 
     expected_output = {
         "name": "chan_name",
@@ -182,16 +172,22 @@ def test_valid_bans_in_public(clear, user1, user2, user3):
                 'handle_str': 'userone',
                 'profile_img_url': f"{url}/imgurl/default.jpg"     
             },
+            {
+                'u_id': user2["auth_user_id"],
+                'email': 'user2@email.com',
+                'name_first': 'user',
+                'name_last': 'two',
+                'handle_str': 'usertwo',
+                'profile_img_url': f"{url}/imgurl/default.jpg"     
+            },
         ]
     }
 
     assert rh.channel_details(user1["token"], channel_id).json() == expected_output
 
-    join_response = rh.channel_join(user3["token"], channel_id)
-    assert join_response.status_code == 403
-
-def test_valid_bans_in_private(clear, user1, user2, user3):
-    channel_id = create_channel(user1["token"], [user2["auth_user_id"], user3["auth_user_id"]], False)
+def test_valid_unban_in_private(clear, user1, user2):
+    channel_id = rh.channels_create(user1["token"], "chan_name", False).json()['channel_id']
+    rh.channel_invite(user1["token"], channel_id, user2["auth_user_id"])
 
     expected_output = {
         "name": "chan_name",
@@ -223,17 +219,9 @@ def test_valid_bans_in_private(clear, user1, user2, user3):
                 'handle_str': 'usertwo',
                 'profile_img_url': f"{url}/imgurl/default.jpg"     
             },
-            {
-                'u_id': user3["auth_user_id"],
-                'email': 'user3@email.com',
-                'name_first': 'user',
-                'name_last': 'three',
-                'handle_str': 'userthree',
-                'profile_img_url': f"{url}/imgurl/default.jpg"     
-            },
         ]
     }
-    print(rh.channel_details(user1["token"], channel_id).json())
+
     assert rh.channel_details(user1["token"], channel_id).json() == expected_output
 
     ban_response = rh.channel_ban(user1["token"], channel_id, user2["auth_user_id"])
@@ -261,28 +249,23 @@ def test_valid_bans_in_private(clear, user1, user2, user3):
                 'handle_str': 'userone',
                 'profile_img_url': f"{url}/imgurl/default.jpg"     
             },
-            {
-                'u_id': user3["auth_user_id"],
-                'email': 'user3@email.com',
-                'name_first': 'user',
-                'name_last': 'three',
-                'handle_str': 'userthree',
-                'profile_img_url': f"{url}/imgurl/default.jpg"     
-            },
         ]
     }
 
     assert rh.channel_details(user1["token"], channel_id).json() == expected_output
 
-    join_response = rh.channel_invite(user1["token"], channel_id, user2["auth_user_id"])
-    assert join_response.status_code == 400
+    invite_response = rh.channel_invite(user1["token"], channel_id, user2["auth_user_id"])
+    assert invite_response.status_code == 400
 
-    ban_response = rh.channel_ban(user1["token"], channel_id, user3["auth_user_id"])
-    assert ban_response.status_code == 200
+    unban_response = rh.channel_unban(user1["token"], channel_id, user2["auth_user_id"])
+    assert unban_response.status_code == 200
+
+    invite_response = rh.channel_invite(user1["token"], channel_id, user2["auth_user_id"])
+    assert invite_response.status_code == 400
 
     expected_output = {
         "name": "chan_name",
-        "is_public": False,
+        "is_public": True,
         "owner_members": [
             {
                 'u_id': user1["auth_user_id"],
@@ -302,10 +285,15 @@ def test_valid_bans_in_private(clear, user1, user2, user3):
                 'handle_str': 'userone',
                 'profile_img_url': f"{url}/imgurl/default.jpg"     
             },
+            {
+                'u_id': user2["auth_user_id"],
+                'email': 'user2@email.com',
+                'name_first': 'user',
+                'name_last': 'two',
+                'handle_str': 'usertwo',
+                'profile_img_url': f"{url}/imgurl/default.jpg"     
+            },
         ]
     }
 
     assert rh.channel_details(user1["token"], channel_id).json() == expected_output
-
-    join_response = rh.channel_invite(user1["token"], channel_id, user3["auth_user_id"])
-    assert join_response.status_code == 400
